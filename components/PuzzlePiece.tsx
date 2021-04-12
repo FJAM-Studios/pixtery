d3d7f58201558ad2e3071541547858f14d979f85
Fu Goto <fu.goto@gmail.com>
2021-04-12 00:34:48 -0400

    refactored code

--- components/PuzzlePiece.tsx
+++ components/PuzzlePiece.tsx
@@ -39,7 +39,7 @@ export default ({
   currentBoard: (number | null)[];
   setCurrentBoard: Function;
   setErrorMessage: Function;
-  puzzleAreaDimensions: any // change later
+  puzzleAreaDimensions: { puzzleAreaWidth: number, puzzleAreaHeight: number }
 }) => {  
   const { puzzleAreaWidth, puzzleAreaHeight } = puzzleAreaDimensions;
   const minSandboxY = boardSize * 1.05;
@@ -111,8 +111,6 @@ export default ({
   }, []);
 
   const changePosition = (gestureState: { dx: number; dy: number }): void => {
-    console.log('num',num, 'previx', prevIx, 'newsnapix', currentSnappedIx, 'board before', currentBoard)
-    console.log(squareSize, gridSections)
     setErrorMessage("");
     //update the relative _x and _y but leave x and y the same unless snapping
     const newXY = {
@@ -123,66 +121,67 @@ export default ({
       // snapAdjusted_x: currentXY.snapAdjusted_x + gestureState.dx,
       // snapAdjusted_y: currentXY.snapAdjusted_y + gestureState.dy
     };
-    console.log('currenty', newXY.y, 'currentx', newXY.x)
-    console.log('current_y', currentXY._y, 'current_x', currentXY._x)
-    console.log('dy', gestureState.dy, 'dx', gestureState.dx)
-    console.log('absY', newXY._y, 'absX', newXY._x)
 
-    //if _x and _y are within a margin of a point on the grid, then snap!
-    let snappedX: number | undefined; // top left X position of snap grid
-    let snappedY: number | undefined; // top left Y position of snap grid
-    let snappedRow: number | undefined; // row index where it snaps
-    let snappedCol: number | undefined; // col index where it snaps
-    const rowDividers: number[] = gridSections.rowDividers;
-    for (let i = 0; i < rowDividers.length; i++) {
-      const rowDivider = rowDividers[i];
-      if (Math.abs(newXY._y - rowDivider) < squareSize * SNAP_MARGIN) {
-        console.log('row margin from divider', newXY._y - rowDivider)
-        snappedY = initY - newXY._y + rowDivider;
-        snappedRow = i;
-        break;
-      }
-    }
-    const colDividers: number[] = gridSections.colDividers;
-    for (let i = 0; i < colDividers.length; i++) {
-      const colDivider = colDividers[i];
-      if (Math.abs(newXY._x - colDivider) < squareSize * SNAP_MARGIN) {
-        console.log('col margin from divider', newXY._x - colDivider)
-        snappedX = initX - newXY._x + colDivider;
-        snappedCol = i;
-        break;
-      }
-    }
+    // snappedX: top left X position of snap grid
+    // snappedY: top left Y position of snap grid
+    // snappedRow: row index where it snaps
+    // snappedCol: col index where it snaps
+    const [snappedX, snappedY, snappedRow, snappedCol] = determineSnap(newXY)
+
     let newIx: number | undefined;
-    console.log('snapedY', snappedY, 'snappedx', snappedX)
     // if both snappedX and snapped Y are defined, there was a snap i.e. the piece came within the grid snap margin
     if (snappedX !== undefined && snappedY !== undefined) {
       // putting ! after a variable is to tell TS that in this case, the variable will not be null or undefined
       newIx = snappedRow! * gridSize + snappedCol!;
       if (currentBoard[newIx] === null || newIx === currentSnappedIx) {
-        // console.log('snappedx', snappedX, 'snapy', snappedY)
         newXY.x = snappedX;
         newXY.y = snappedY;
-        // need to adjust accumulated distance if theres a snap?
+        // need to adjust accumulated distance if theres a snap - leave for later
         // newXY.snapAdjusted_y += newXY._y - colDividers[snappedCol!] - (squareSize * SNAP_MARGIN - gestureState.dy)
         // newXY.snapAdjusted_x += newXY._x - rowDividers[snappedRow!] - (squareSize * SNAP_MARGIN - gestureState.dx)
       }
       // but if the current board already has another piece in the new index, do not let user move piece there
       else {
-        console.log('overlap at ',newIx)
         setErrorMessage(
           "There is a piece already in that spot. Please move that piece first!"
         );
         newIx = undefined;
-        // need to check this - ideally would want to send piece back to original location
+        // this is where overlap result would be defined. maybe send piece back to original location, or push other piece out
         // newXY.x = currentXY.x + currentXY._x - gestureState.dx;
         // newXY.y = currentXY.y + currentXY._y - gestureState.dy;
       }
     }
-    console.log('newIx', newIx, 'newsnapix', currentSnappedIx)
     if(newIx !== currentSnappedIx) updateIx(newIx);
     setXY(newXY);
   };
+
+  const determineSnap = (newXY: {x: number, y: number, _x: number, _y: number}) => {
+    let snappedX: number | undefined; // top left X position of snap grid
+    let snappedY: number | undefined; // top left Y position of snap grid
+    let snappedRow: number | undefined; // row index where it snaps
+    let snappedCol: number | undefined; // col index where it snaps
+    const rowDividers: number[] = gridSections.rowDividers;
+    const colDividers: number[] = gridSections.colDividers;
+
+    //if _x and _y are within a margin of a point on the grid, then snap!
+    for (let i = 0; i < rowDividers.length; i++) {
+      const rowDivider = rowDividers[i];
+      if (Math.abs(newXY._y - rowDivider) < squareSize * SNAP_MARGIN) {
+        snappedY = initY - newXY._y + rowDivider;
+        snappedRow = i;
+        break;
+      }
+    }
+    for (let i = 0; i < colDividers.length; i++) {
+      const colDivider = colDividers[i];
+      if (Math.abs(newXY._x - colDivider) < squareSize * SNAP_MARGIN) {
+        snappedX = initX - newXY._x + colDivider;
+        snappedCol = i;
+        break;
+      }
+    }
+    return [snappedX, snappedY, snappedRow, snappedCol]
+  }
 
   // preserve previous Ix, and set the new Ix that it will snap to
   const updateIx = (newIx: number | undefined): void => {
