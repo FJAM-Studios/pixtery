00a03d1be0c66c6a5b9fa99056a0e46cf45f6cf4
Adam Reeves <aar2222@gmail.com>
2021-04-15 09:32:54 -0500

    mark puzzle complete

--- components/Puzzle.tsx
+++ components/Puzzle.tsx
@@ -6,6 +6,7 @@ import Header from "./Header";
 import PuzzlePiece from "./PuzzlePiece";
 import { shuffle, generateJigsawPiecePaths } from "../util";
 import { Puzzle, GridSections } from "../types";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 //disable shuffling for testing
 const disableShuffle = TESTING_MODE;
@@ -16,14 +17,16 @@ export default ({
   navigation,
   receivedPuzzles,
   route,
+  setReceivedPuzzles,
 }: {
   boardSize: number;
   theme: any;
   navigation: any;
   receivedPuzzles: Puzzle[];
   route: any;
+  setReceivedPuzzles: (puzzles: Puzzle[]) => void;
 }) => {
-  const { imageURI, puzzleType, gridSize, message } = route.params;
+  const { imageURI, puzzleType, gridSize, message, publicKey } = route.params;
   const squareSize = boardSize / gridSize;
   const image = { uri: imageURI };
   const [piecePaths, setPiecePaths] = useState(
@@ -31,16 +34,16 @@ export default ({
   );
   const [puzzleAreaDimensions, setPuzzleAreaDimensions] = useState({
     puzzleAreaWidth: 0,
-    puzzleAreaHeight: 0
+    puzzleAreaHeight: 0,
   });
 
   const measurePuzzleArea = (ev: any): void => {
-    if(puzzleAreaDimensions.puzzleAreaHeight) return;
-    setPuzzleAreaDimensions({ 
-        puzzleAreaWidth: ev.nativeEvent.layout.width,
-        puzzleAreaHeight: ev.nativeEvent.layout.height
-      })
-    };
+    if (puzzleAreaDimensions.puzzleAreaHeight) return;
+    setPuzzleAreaDimensions({
+      puzzleAreaWidth: ev.nativeEvent.layout.width,
+      puzzleAreaHeight: ev.nativeEvent.layout.height,
+    });
+  };
   // populates X Y coordinates for upper left corner of each grid section
   const getGridSections = (): GridSections => {
     // separated row and col in case needed for future flexibility
@@ -83,7 +86,7 @@ export default ({
   );
 
   const [currentBoard, setCurrentBoard] = useState<(number | null)[]>(
-    Array(shuffledPieces.length).fill(null),
+    Array(shuffledPieces.length).fill(null)
   );
 
   const [winMessage, setWinMessage] = useState<string>("");
@@ -93,54 +96,63 @@ export default ({
     for (let i = 0; i < currentBoard.length; i++) {
       if (currentBoard[i] !== i) return;
     }
-    const winMessage = message && message.length>0
-      ? message
-      : "Congrats! You solved the puzzle!";
+    const winMessage =
+      message && message.length > 0
+        ? message
+        : "Congrats! You solved the puzzle!";
     setWinMessage(winMessage);
+    markPuzzleComplete(publicKey);
   };
 
-  const [firstSnap, setFirstSnap] = useState(false)
+  const [firstSnap, setFirstSnap] = useState(false);
   const checkFirstSnap = (): void => {
     for (let i = 0; i < currentBoard.length; i++) {
       if (currentBoard[i] !== null) setFirstSnap(true);
     }
-  }
+  };
 
   useEffect(() => {
     checkWin();
-    if(!firstSnap) checkFirstSnap();
+    if (!firstSnap) checkFirstSnap();
   }, [currentBoard]);
 
   const styleProps = {
-    theme, boardSize
-  }
+    theme,
+    boardSize,
+  };
+
+  const markPuzzleComplete = async (key: string) => {
+    const allPuzzles = [
+      ...receivedPuzzles.map((puz) => {
+        return { ...puz, completed: key === publicKey ? true : puz.completed };
+      }),
+    ];
+    await AsyncStorage.setItem("@pixteryPuzzles", JSON.stringify(allPuzzles));
+    setReceivedPuzzles(allPuzzles);
+  };
 
   // need to return dummy component to measure the puzzle area via onLayout
-  if(!puzzleAreaDimensions.puzzleAreaHeight) return (
-    <SafeAreaView
-      style={styles(styleProps).parentContainer}
-    >
-    <Header
-      theme={theme}
-      notifications={
-        receivedPuzzles.filter((puzzle) => !puzzle.completed).length
-      }
-      navigation={navigation}
-    />
-    <View
-      onLayout={(ev) => measurePuzzleArea(ev)}
-      style={{
-        flex: 1,
-        justifyContent: "flex-end",
-      }}
-    >
-    </View>
-  </SafeAreaView>
-  )
+  if (!puzzleAreaDimensions.puzzleAreaHeight)
+    return (
+      <SafeAreaView style={styles(styleProps).parentContainer}>
+        <Header
+          theme={theme}
+          notifications={
+            receivedPuzzles.filter((puzzle) => !puzzle.completed).length
+          }
+          navigation={navigation}
+        />
+        <View
+          onLayout={(ev) => measurePuzzleArea(ev)}
+          style={{
+            flex: 1,
+            justifyContent: "flex-end",
+          }}
+        ></View>
+      </SafeAreaView>
+    );
   return (
-    <SafeAreaView
-      style={styles(styleProps).parentContainer}
-    >
+    <SafeAreaView style={styles(styleProps).parentContainer}>
       <Header
         theme={theme}
         notifications={
@@ -154,40 +166,45 @@ export default ({
           justifyContent: "flex-end",
         }}
       >
-        <View
-          style={styles(styleProps).puzzleArea}
-        >
+        <View style={styles(styleProps).puzzleArea}>
           <View style={styles(styleProps).messageContainer}>
-            { !firstSnap ? 
-              <Text style={styles(styleProps).startText}>Move pieces onto this board!</Text>
-              : null }
+            {!firstSnap ? (
+              <Text style={styles(styleProps).startText}>
+                Move pieces onto this board!
+              </Text>
+            ) : null}
           </View>
         </View>
-        {!winMessage ? 
-        shuffledPieces.map((num: number, ix: number) => (
-          <PuzzlePiece
-            key={num}
-            num={num}
-            ix={ix}
-            gridSize={gridSize}
-            squareSize={squareSize}
-            puzzleType={puzzleType}
-            image={image}
-            piecePath={piecePaths[num]}
-            boardSize={boardSize}
-            gridSections={gridSections}
-            currentBoard={currentBoard}
-            setCurrentBoard={setCurrentBoard}
-            setErrorMessage={setErrorMessage}
-            puzzleAreaDimensions={puzzleAreaDimensions}
-          />
-        )) 
-        : <Image 
-            source={{uri: imageURI}}
-            style={{width: boardSize, height: boardSize, position: "absolute",
-            top: "0%"}}
+        {!winMessage ? (
+          shuffledPieces.map((num: number, ix: number) => (
+            <PuzzlePiece
+              key={num}
+              num={num}
+              ix={ix}
+              gridSize={gridSize}
+              squareSize={squareSize}
+              puzzleType={puzzleType}
+              image={image}
+              piecePath={piecePaths[num]}
+              boardSize={boardSize}
+              gridSections={gridSections}
+              currentBoard={currentBoard}
+              setCurrentBoard={setCurrentBoard}
+              setErrorMessage={setErrorMessage}
+              puzzleAreaDimensions={puzzleAreaDimensions}
             />
-        }
+          ))
+        ) : (
+          <Image
+            source={{ uri: imageURI }}
+            style={{
+              width: boardSize,
+              height: boardSize,
+              position: "absolute",
+              top: "0%",
+            }}
+          />
+        )}
         <View style={styles(styleProps).messageContainer}>
           <Text style={styles(styleProps).winText}>{winMessage}</Text>
         </View>
@@ -199,46 +216,47 @@ export default ({
   );
 };
 
-const styles = (props: any) => StyleSheet.create({
-  messageContainer: {
-    flexDirection: "row",
-    zIndex: -1,
-  },
-  errorText: {
-    fontSize: 20,
-    flexWrap: "wrap",
-    textAlign: "center",
-    flex: 1,
-    color: "orange",
-  },
-  winText: {
-    fontSize: 20,
-    flexWrap: "wrap",
-    textAlign: "center",
-    flex: 1,
-    color: "white",
-  },
-  startText: {
-    fontSize: 20,
-    flexWrap: "wrap",
-    textAlign: "center",
-    flex: 1,
-    color: "white",
-  },
-  puzzleArea: { 
-    width: props.boardSize,
-    height: props.boardSize,
-    borderWidth: 4,
-    borderColor: "white",
-    position: "absolute",
-    top: "0%",
-    justifyContent: "center"
-  },
-  parentContainer: {
-    flex: 1,
-    flexDirection: "column",
-    padding: 10,
-    backgroundColor: props.theme.colors.background,
-    justifyContent: "flex-start",
-  }
-});
+const styles = (props: any) =>
+  StyleSheet.create({
+    messageContainer: {
+      flexDirection: "row",
+      zIndex: -1,
+    },
+    errorText: {
+      fontSize: 20,
+      flexWrap: "wrap",
+      textAlign: "center",
+      flex: 1,
+      color: "orange",
+    },
+    winText: {
+      fontSize: 20,
+      flexWrap: "wrap",
+      textAlign: "center",
+      flex: 1,
+      color: "white",
+    },
+    startText: {
+      fontSize: 20,
+      flexWrap: "wrap",
+      textAlign: "center",
+      flex: 1,
+      color: "white",
+    },
+    puzzleArea: {
+      width: props.boardSize,
+      height: props.boardSize,
+      borderWidth: 4,
+      borderColor: "white",
+      position: "absolute",
+      top: "0%",
+      justifyContent: "center",
+    },
+    parentContainer: {
+      flex: 1,
+      flexDirection: "column",
+      padding: 10,
+      backgroundColor: props.theme.colors.background,
+      justifyContent: "flex-start",
+    },
+  });
