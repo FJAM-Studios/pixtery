f357a778dae5aa99c76b132ca6d7412ab2a38b00
Fu Goto <fu.goto@gmail.com>
2021-04-18 16:15:55 -0400

    upload puzzle settings cloud

--- FirebaseApp.ts
+++ FirebaseApp.ts
@@ -23,13 +23,10 @@ const initializeApp = (): any => {
 const app = initializeApp();
 const db = app.firestore();
 
-// start here - db emulator
+// for host, put in host: <Metro Bundler LAN IP address>:8080
 db.settings({ host: "192.168.1.215:8080", ssl: false });
 let functions = app.functions()
-// functions.useFunctionsEmulator("http://localhost:5001")
-// functions.useFunctionsEmulator("http://127.0.0.1:5001")
-// functions.useFunctionsEmulator("http://192.168.1.69:5001")
-// Metro Bundler LAN IP address
+// for http, put in http: <Metro Bundler LAN IP address>:5001
 functions.useFunctionsEmulator("http://192.168.1.215:5001")
 
 const storage = app.storage();
@@ -39,27 +36,5 @@ const verifySms = (id: string, code: string) => {
   const signInResponse = firebase.auth().signInWithCredential(credential);
   return signInResponse;
 };
-// const functions = firebase.functions().useEmulator("localhost", 5001);
-// let functions = firebase.functions()
-// functions.useFunctionsEmulator("http://localhost:5001")
-
-// const addNumbers = app.functions().httpsCallable("addNumbers")
-
-// addNumbers({firstNumber: 1, secondNumber: 2})
-//     .then((result: any) => {
-//     console.log('result test in firebaseapp', result)
-//   }).catch((e: any) => {
-//     console.log('there is error in firebaseapp')
-//     console.error('message', e.message)
-//     console.error('code', e.code)
-//     console.error('details', e.details)
-//   })
 
 export { app, db, storage, phoneProvider, firebaseConfig, verifySms, functions };
-
-// right now expo image manipulater wont take a url for source
-// function on our domain that gets image from cloud storage 
-// right now url is going direct to image manipulator
-// want the cloud function to grab image from cloud storage and send to client - cloud function sits on website domain 
-// cloud function should also obscure data
-// need cloud function to rplace requestimage - get the image from cloud and directly send to client
