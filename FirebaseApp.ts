2d9709086e5746c2c10b844b070b3ccc4a070ef5
Fu Goto <fu.goto@gmail.com>
2021-04-19 15:25:58 -0400

    query puzzle url

--- FirebaseApp.ts
+++ FirebaseApp.ts
@@ -23,12 +23,9 @@ const initializeApp = (): any => {
 const app = initializeApp();
 const db = app.firestore();
 
-// for host, put in host: <Metro Bundler LAN IP address>:8080
-// db.settings({ host: "192.168.1.215:8080", ssl: false });
 let functions = app.functions()
 // for http, put in http: <Metro Bundler LAN IP address>:5001
 functions.useFunctionsEmulator("http://192.168.1.215:5001")
-// functions.useEmulator("localhost", 5001)
 
 const storage = app.storage();
 const phoneProvider = new firebase.auth.PhoneAuthProvider();
@@ -38,20 +35,17 @@ const verifySms = (id: string, code: string) => {
   return signInResponse;
 };
 
-// const functions = firebase.functions().useEmulator("localhost", 5001);
-// let functions = firebase.functions()
-// functions.useFunctionsEmulator("http://localhost:5001")
+// test to see if cloud funcs are running
+// const addNumbers = app.functions().httpsCallable("addNumbers")
 
-const addNumbers = app.functions().httpsCallable("addNumbers")
-
-addNumbers({firstNumber: 1, secondNumber: 2})
-    .then((result: any) => {
-    console.log('result test in firebaseapp', result)
-  }).catch((e: any) => {
-    console.log('there is error in firebaseapp')
-    console.error('message', e.message)
-    console.error('code', e.code)
-    console.error('details', e.details)
-  })
+// addNumbers({firstNumber: 1, secondNumber: 2})
+//     .then((result: any) => {
+//     console.log('result test in firebaseapp', result)
+//   }).catch((e: any) => {
+//     console.log('there is error in firebaseapp')
+//     console.error('message', e.message)
+//     console.error('code', e.code)
+//     console.error('details', e.details)
+//   })
 
 export { app, db, storage, phoneProvider, firebaseConfig, verifySms, functions };
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,82 +2,10 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import * as functions from "firebase-functions";
 import {db} from "../../FirebaseApp";
-import {Puzzle as PuzzleType} from "../../types";
-
-
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
-// Take the text parameter passed to this HTTP endpoint and insert it into 
-// Firestore under the path /messages/:documentId/original
-
-// exports.addMessage = functions.https.onRequest(async (req, res) => {
-//   // Grab the text parameter.
-//   const original = req.query.text
-//   // Push the new message into Firestore using the Firebase Admin SDK.
-//   const writeResult = await db.collection("messages").add({original: original});
-//   // Send back a message that we've successfully written the message
-//   console.log("adding message")
-//   res.json({result: `Message with ID: ${writeResult.id} added.`});
-// })
-
-// Listens for new messages added to /messages/:documentId/original and creates an
-// uppercase version of the message to /messages/:documentId/uppercase
-// exports.makeUppercase = functions.firestore.document("/messages/{documentId}")
-//     .onCreate((snap, context) => {
-//       console.log("uppercase running")
-//       // Grab the current value of what was written to Firestore.
-//       const original = snap.data().original;
-
-//       // Access the parameter `{documentId}` with `context.params`
-//       functions.logger.log("Uppercasing", context.params.documentId, original);
-      
-//       const uppercase = original.toUpperCase();
-      
-//       // You must return a Promise when performing asynchronous tasks inside a Functions such as
-//       // writing to Firestore.
-//       // Setting an 'uppercase' field in Firestore document returns a Promise.
-//       return snap.ref.set({uppercase}, {merge: true});
-//     });
-
-// exports.test = functions.https.onCall(async (data) => {
-//   console.log('hi')
-//   try {
-//     console.log("hello world")
-//     return {text: data.text};
-//   }
-//   // [END returnMessageAsync]
-//   catch (error) {
-//   // Re-throwing the error as an HttpsError so that the client gets the error details.
-//     throw new functions.https.HttpsError("unknown", error.message, error);
-//   }
-// });
-// start here - make this a then ca
-// exports.addMessageCall = functions.https.onCall(async (data: any) => {
-//   // Grab the text parameter.
-//   try{
-//     const original = data.text;
-//     // Push the new message into Firestore using the Firebase Admin SDK.
-//     const writeResult = await db.collection("messages").add({original: original});
-//     // Send back a message that we've successfully written the message
-//     console.log("adding message")
-//     return {result: writeResult}
-//   }
-//   catch (error){
-//     console.log("error")
-//     throw new functions.https.HttpsError("unknown", error.message, error);
-//   }
-// })
 
 exports.uploadPuzzleSettings = functions.https.onCall(async (data: { fileName: string; puzzleType: string; gridSize: number; profile: any; message: string; publicKey: string; }) => {
   const {fileName, puzzleType, gridSize, profile, message, publicKey} = data;
-  // cloud function can hide this code collection bc its written on "server"
-  console.log("uploading puzz settings");
+  console.log("uploading puzzle settings");
   try {
     await db
         .collection("puzzles")
@@ -98,7 +26,8 @@ exports.uploadPuzzleSettings = functions.https.onCall(async (data: { fileName: s
   }
 });
 
-exports.queryPuzzle = functions.https.onCall(async (data) : Promise<PuzzleType | void>=> {
+// return type set as an generic object bc a JSON is returned (Puzzle type is nested in that)
+exports.queryPuzzle = functions.https.onCall(async (data) : Promise<Record<string, any> | void>=> {
   try {
     const {publicKey} = data;
     const snapshot = await db
@@ -109,8 +38,8 @@ exports.queryPuzzle = functions.https.onCall(async (data) : Promise<PuzzleType |
       console.log("no puzzle found!");
       throw new functions.https.HttpsError("not-found", "no puzzle found!");
     } else {
-    //does this do anything? puzzleData is overwritten immediately below
-      let puzzleData: PuzzleType = {
+    // does this do anything? puzzleData is overwritten immediately below
+      let puzzleData: Record<string, any> = {
         puzzleType: "",
         gridSize: 0,
         senderName: "",
@@ -120,12 +49,11 @@ exports.queryPuzzle = functions.https.onCall(async (data) : Promise<PuzzleType |
         dateReceived: "",
         completed: false,
       };
-      //NOTE: there SHOULD only be one puzzle but it's in an object that has to iterated through to access the data
+      // NOTE: there SHOULD only be one puzzle but it's in an object that has to iterated through to access the data
       snapshot.forEach((puzzle: any) => {
         puzzleData = puzzle.data();
         puzzleData.completed = false;
       });
-      console.log("retrieved puzzle data", puzzleData);
       return puzzleData;
     }
   } catch (error) {
@@ -133,25 +61,18 @@ exports.queryPuzzle = functions.https.onCall(async (data) : Promise<PuzzleType |
   }
 });
 
+// for testing cloud function connectivity on client
 exports.addNumbers = functions.https.onCall((data: { firstNumber: any; secondNumber: any; }) => {
-  console.log("adding nums")
-  // [END addFunctionTrigger]
-  // [START readAddData]
+  console.log("adding nums");
   // Numbers passed from the client.
   const firstNumber = data.firstNumber;
   const secondNumber = data.secondNumber;
-  // [END readAddData]
-    
-  // [START addHttpsError]
   // Checking that attributes are present and are numbers.
   if (!Number.isFinite(firstNumber) || !Number.isFinite(secondNumber)) {
     // Throwing an HttpsError so that the client gets the error details.
     throw new functions.https.HttpsError("invalid-argument", "The function must be called with " +
           "two arguments \"firstNumber\" and \"secondNumber\" which must both be numbers.");
   }
-  // [END addHttpsError]
-  
-  // [START returnAddData]
   // returning result.
   return {
     firstNumber: firstNumber,
@@ -159,5 +80,4 @@ exports.addNumbers = functions.https.onCall((data: { firstNumber: any; secondNum
     operator: "+",
     operationResult: firstNumber + secondNumber,
   };
-  // [END returnAddData]
 });
