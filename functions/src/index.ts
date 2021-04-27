51df96e25f85813f09e1eedf7125d4603eefa113
mxavier927 <mxavier927@gmail.com>
2021-04-26 20:53:52 -0400

    for real added functions to gitignore

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,55 +1,55 @@
 /* eslint-disable max-len */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import * as functions from "firebase-functions";
-import { db } from "../../FirebaseApp";
-import { Puzzle } from "../../types";
+import {db} from "../../FirebaseApp";
+import {Puzzle} from "../../types";
 
 exports.uploadPuzzleSettings = functions.https.onCall(
-  async (data: { fileName: string; newPuzzle: Puzzle }) => {
-    const { fileName, newPuzzle } = data;
-    console.log("uploading puzzle settings");
-    try {
-      await db.collection("puzzles").doc(fileName).set(newPuzzle);
-      return { result: `successfully uploaded ${fileName}` };
-    } catch (error) {
-      throw new functions.https.HttpsError("unknown", error.message, error);
+    async (data: { fileName: string; newPuzzle: Puzzle }) => {
+      const {fileName, newPuzzle} = data;
+      console.log("uploading puzzle settings");
+      try {
+        await db.collection("puzzles").doc(fileName).set(newPuzzle);
+        return {result: `successfully uploaded ${fileName}`};
+      } catch (error) {
+        throw new functions.https.HttpsError("unknown", error.message, error);
+      }
     }
-  }
 );
 
 // return type set as an generic object bc a JSON is returned (Puzzle type is nested in that)
 exports.queryPuzzle = functions.https.onCall(
-  async (data): Promise<Record<string, any> | void> => {
-    try {
-      const { publicKey } = data;
-      const snapshot = await db
-        .collection("puzzles")
-        .where("publicKey", "==", publicKey)
-        .get();
-      if (snapshot.empty) {
-        console.log("no puzzle found!");
-        throw new functions.https.HttpsError("not-found", "no puzzle found!");
-      } else {
+    async (data): Promise<Record<string, any> | void> => {
+      try {
+        const {publicKey} = data;
+        const snapshot = await db
+            .collection("puzzles")
+            .where("publicKey", "==", publicKey)
+            .get();
+        if (snapshot.empty) {
+          console.log("no puzzle found!");
+          throw new functions.https.HttpsError("not-found", "no puzzle found!");
+        } else {
         // does this do anything? puzzleData is overwritten immediately below
-        let puzzleData: Record<string, any> = {
-          puzzleType: "",
-          gridSize: 0,
-          senderName: "",
-          senderPhone: "string",
-          imageURI: "",
-          message: null,
-          dateReceived: "",
-          completed: false,
-        };
-        // NOTE: there SHOULD only be one puzzle but it's in an object that has to iterated through to access the data
-        snapshot.forEach((puzzle: any) => {
-          puzzleData = puzzle.data();
-          puzzleData.completed = false;
-        });
-        return puzzleData;
+          let puzzleData: Record<string, any> = {
+            puzzleType: "",
+            gridSize: 0,
+            senderName: "",
+            senderPhone: "string",
+            imageURI: "",
+            message: null,
+            dateReceived: "",
+            completed: false,
+          };
+          // NOTE: there SHOULD only be one puzzle but it's in an object that has to iterated through to access the data
+          snapshot.forEach((puzzle: any) => {
+            puzzleData = puzzle.data();
+            puzzleData.completed = false;
+          });
+          return puzzleData;
+        }
+      } catch (error) {
+        throw new functions.https.HttpsError("unknown", error.message, error);
       }
-    } catch (error) {
-      throw new functions.https.HttpsError("unknown", error.message, error);
     }
-  }
 );
