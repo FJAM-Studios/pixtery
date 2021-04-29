35c32cb422aa44f347e572ebbbafd7ed047c8b13
Adam Reeves <aar2222@gmail.com>
2021-04-29 17:49:58 -0500

    Draggable replacement with partial rotation

--- components/PuzzlePiece.tsx
+++ components/PuzzlePiece.tsx
@@ -1,11 +1,25 @@
 import * as ImageManipulator from "expo-image-manipulator";
-import React, { useState, useEffect } from "react";
+import React, { useRef, createRef } from "react";
 import { Svg, Image, Defs, ClipPath, Path, Rect } from "react-native-svg";
+import { Animated, View, StyleSheet, PanResponder, Text } from "react-native";
+import {
+  PanGestureHandler,
+  State,
+  PanGestureHandlerStateChangeEvent,
+  RotationGestureHandler,
+  RotationGestureHandlerStateChangeEvent,
+  RotationGestureHandlerGestureEvent,
+} from "react-native-gesture-handler";
 
-import { SNAP_MARGIN } from "../constants";
+import {
+  SNAP_MARGIN,
+  DEGREE_CONVERSION,
+  USE_NATIVE_DRIVER,
+} from "../constants";
 import { GridSections } from "../types";
 import { getInitialDimensions } from "../util";
 import Draggable from "./CustomDraggable";
+const AnimatedSvg = Animated.createAnimatedComponent(Svg);
 
 export default ({
   num,
@@ -22,7 +36,7 @@ export default ({
   setErrorMessage,
   puzzleAreaDimensions,
   z,
-  moveToTop
+  moveToTop,
 }: {
   num: number;
   ix: number;
@@ -40,246 +54,119 @@ export default ({
   z: number;
   moveToTop: Function;
 }): JSX.Element | null => {
-  const { puzzleAreaWidth, puzzleAreaHeight } = puzzleAreaDimensions;
-  const minSandboxY = boardSize * 1.05;
-  const maxSandboxY = puzzleAreaHeight - squareSize;
-  //squareX and squareY represent the row and col of the square in the solved puzzle
-  //widthX and widthY are the size of the pieces (larger for jigsaw);
-  //initX and initY are starting position for pieces (not aligned w grid for jigsaw)
-  //viewBoxX and viewBoxY are 'panned' for selecting correct portion of image for piece
-  //solutionX and solutionY are the top left coords for where the piece belongs in solution
-  const [
-    squareX,
-    squareY,
-    widthY,
-    widthX,
-    initX,
-    initY,
-    viewBoxX,
-    viewBoxY,
-    solutionX,
-    solutionY,
-  ] = getInitialDimensions(
-    puzzleType,
-    minSandboxY,
-    maxSandboxY,
-    num,
-    ix,
-    gridSize,
-    squareSize
+  // these refs are only relevant if we decide to allow simultaneous drag and rotate,
+  // which I feel is somewhat awkward to use
+
+  // const moveRef = createRef<PanGestureHandler>();
+  // const rotationRef = createRef<RotationGestureHandler>();
+
+  const pan = useRef(new Animated.ValueXY()).current;
+  const lastOffset = { x: 0, y: 0 };
+
+  const rotate = useRef(new Animated.Value(0)).current;
+  const rotateStr = rotate.interpolate({
+    inputRange: [-100, 100],
+    outputRange: ["-100rad", "100rad"],
+  });
+  let lastRotate = 0;
+
+  const onGestureEvent = Animated.event(
+    [{ nativeEvent: { translationX: pan.x, translationY: pan.y } }],
+    { useNativeDriver: USE_NATIVE_DRIVER }
   );
 
-  const [ready, setReady] = useState<boolean>(false);
-  const [croppedImage, setCroppedImage] = useState({ uri: imageURI });
-  const [currentSnappedIx, setCurrentSnappedIx] = useState<
-    number | undefined | null
-  >(-1);
-  // previous index is needed to know where the piece moved from, to update to null on current board
-  const [prevIx, setPrevIx] = useState<number | undefined | null>(null);
-
-  //_x and _y are used to keep track of where image is relative to its start positon
-  const [currentXY, setXY] = useState({
-    x: initX,
-    y: initY,
-    _x: initX, // to track cumulative X distance traveled from original position
-    _y: initY, // to track cumulative Y distance traveled from original position
-    _xOffset: initX, //to track distance offset from snapping X
-    _yOffset: initY, //to track distance offset from snapping Y
-  });
-
-  useEffect(() => {
-    const manipulateImage = async () => {
-      setReady(false);
-      const croppedImage = await ImageManipulator.manipulateAsync(
-        imageURI,
-        [
-          {
-            resize: {
-              width: boardSize,
-              height: boardSize,
-            },
-          },
-          {
-            crop: {
-              originX: viewBoxX,
-              originY: viewBoxY,
-              width: widthX,
-              height: widthY,
-            },
-          },
-        ],
-        { compress: 1, format: ImageManipulator.SaveFormat.JPEG }
-      );
-      setCroppedImage(croppedImage);
-      setReady(true);
-    };
-    manipulateImage();
-    setCurrentSnappedIx(-1);
-    setPrevIx(null);
-    setXY({
-      x: initX,
-      y: initY,
-      _x: initX,
-      _y: initY,
-      _xOffset: initX,
-      _yOffset: initY,
-    });
-  }, [imageURI]);
-
-  const changePosition = (gestureState: {
-    dx: number;
-    dy: number;
-    moveX: number;
-    moveY: number;
-  }): void => {
-    setErrorMessage("");
-    //update the relative _x and _y but leave x and y the same unless snapping
-    const newXY = {
-      x: currentXY.x,
-      y: currentXY.y,
-      _x: currentXY._x + gestureState.dx,
-      _y: currentXY._y + gestureState.dy,
-      _xOffset: currentXY._xOffset + gestureState.dx,
-      _yOffset: currentXY._yOffset + gestureState.dy,
-      // snapAdjusted_x: currentXY.snapAdjusted_x + gestureState.dx,
-      // snapAdjusted_y: currentXY.snapAdjusted_y + gestureState.dy
-    };
-    // snappedX: top left X position of snap grid
-    // snappedY: top left Y position of snap grid
-    // snappedRow: row index where it snaps
-    // snappedCol: col index where it snaps
-    const [snappedX, snappedY, snappedRow, snappedCol] = determineSnap(newXY);
-
-    let newIx: number | undefined;
-    // if both snappedX and snapped Y are defined, there was a snap i.e. the piece came within the grid snap margin
-    if (snappedX !== undefined && snappedY !== undefined) {
-      // putting ! after a variable is to tell TS that in this case, the variable will not be null or undefined
-      newIx = snappedRow! * gridSize + snappedCol!;
-      if (currentBoard[newIx] === null || newIx === currentSnappedIx) {
-        //update the offset to keep track of the 'snapped' distance for subsequent snapping calculations
-        newXY._xOffset += snappedX - newXY.x;
-        newXY._yOffset += snappedY - newXY.y;
-        newXY.x = snappedX;
-        newXY.y = snappedY;
-        // newXY._x = snappedRow * gridSize
-        // newXY._y = snappedCol * gridSize
-        // need to adjust accumulated distance if theres a snap - leave for later
-        // newXY.snapAdjusted_y += newXY._y - colDividers[snappedCol!] - (squareSize * SNAP_MARGIN - gestureState.dy)
-        // newXY.snapAdjusted_x += newXY._x - rowDividers[snappedRow!] - (squareSize * SNAP_MARGIN - gestureState.dx)
-      }
-      // but if the current board already has another piece in the new index, do not let user move piece there
-      else {
-        setErrorMessage(
-          "There is a piece already in that spot. Please move that piece first!"
-        );
-        newIx = undefined;
-        // this is where overlap result would be defined. maybe send piece back to original location, or push other piece out
-        // newXY.x = currentXY.x + currentXY._x - gestureState.dx;
-        // newXY.y = currentXY.y + currentXY._y - gestureState.dy;
-      }
+  const onHandlerStateChange = (ev: PanGestureHandlerStateChangeEvent) => {
+    if (ev.nativeEvent.oldState === State.ACTIVE) {
+      lastOffset.x += ev.nativeEvent.translationX;
+      lastOffset.y += ev.nativeEvent.translationY;
+      pan.setOffset(lastOffset);
+      pan.setValue({ x: 0, y: 0 });
     }
-
-    if (newIx !== currentSnappedIx) updateIx(newIx);
-    setXY(newXY);
   };
 
-  const determineSnap = (newXY: {
-    x: number;
-    y: number;
-    _x: number;
-    _y: number;
-    _xOffset: number;
-    _yOffset: number;
-  }) => {
-    let snappedX: number | undefined; // top left X position of snap grid
-    let snappedY: number | undefined; // top left Y position of snap grid
-    let snappedRow: number | undefined; // row index where it snaps
-    let snappedCol: number | undefined; // col index where it snaps
-    const rowDividers: number[] = gridSections.rowDividers;
-    const colDividers: number[] = gridSections.colDividers;
-    //if _x and _y are within a margin of a point on the grid, then snap!
-    for (let i = 0; i < rowDividers.length; i++) {
-      const rowDivider = rowDividers[i];
-      if (Math.abs(newXY._yOffset - rowDivider) < squareSize * SNAP_MARGIN) {
-        snappedY = initY - newXY._y + rowDivider;
-        snappedRow = i;
-        break;
-      }
+  const onRotateGestureEvent = Animated.event(
+    [{ nativeEvent: { rotation: rotate } }],
+    {
+      useNativeDriver: USE_NATIVE_DRIVER,
     }
-    for (let i = 0; i < colDividers.length; i++) {
-      const colDivider = colDividers[i];
-      if (Math.abs(newXY._xOffset - colDivider) < squareSize * SNAP_MARGIN) {
-        snappedX = initX - newXY._x + colDivider;
-        snappedCol = i;
-        break;
+  );
+
+  const onRotateHandlerStateChange = (
+    ev: RotationGestureHandlerStateChangeEvent
+  ) => {
+    if (ev.nativeEvent.oldState === State.ACTIVE) {
+      lastRotate += ev.nativeEvent.rotation;
+      // convert rotation to between 0 and 2 * pi
+      lastRotate = lastRotate % (Math.PI * 2);
+      lastRotate += 2 * Math.PI;
+      lastRotate = lastRotate % (Math.PI * 2);
+      // 'snap' rotation to whichever angle it's closest to
+      if (
+        lastRotate >= 315 * DEGREE_CONVERSION ||
+        lastRotate < 45 * DEGREE_CONVERSION
+      ) {
+        lastRotate = 0;
+      } else if (
+        lastRotate >= 45 * DEGREE_CONVERSION &&
+        lastRotate < 135 * DEGREE_CONVERSION
+      ) {
+        lastRotate = 90 * DEGREE_CONVERSION;
+      } else if (
+        lastRotate >= 135 * DEGREE_CONVERSION &&
+        lastRotate < 225 * DEGREE_CONVERSION
+      ) {
+        lastRotate = 180 * DEGREE_CONVERSION;
+      } else {
+        lastRotate = 270 * DEGREE_CONVERSION;
       }
+      rotate.setOffset(lastRotate);
+      rotate.setValue(0);
     }
-    return [snappedX, snappedY, snappedRow, snappedCol];
   };
-
-  // preserve previous Ix, and set the new Ix that it will snap to
-  const updateIx = (newIx: number | undefined): void => {
-    // if currentSnappedIx has been already set once before, save that index as the previous index
-    if (currentSnappedIx !== -1) setPrevIx(currentSnappedIx);
-    setCurrentSnappedIx(newIx);
-  };
-
-  const updateCurrentBoard = (): void => {
-    const newBoard = [...currentBoard];
-    // putting ! after a variable is to tell TS that in this case, the variable will not be null or undefined
-    if (currentSnappedIx! >= 0) newBoard[currentSnappedIx!] = num;
-    if (prevIx! >= 0 && prevIx !== currentSnappedIx) newBoard[prevIx!] = null;
-    setCurrentBoard(newBoard);
-  };
-
-  useEffect(() => {
-    // if the piece has been mounted already (i.e. currentSnappedIx is not -1), update current board after currentXY changes
-    if (currentSnappedIx !== -1) updateCurrentBoard();
-  }, [currentSnappedIx]);
-
-  if (!ready) return null;
 
   return (
-    <Draggable
-      //draggable SVG needs to be placed where cropped image starts. jigsaw shape extends beyond square
-      x={currentXY.x}
-      y={currentXY.y}
-      z={z}
-      //on release of a piece, update the state and check for snapping
-      onPressIn={() => moveToTop(ix)}
-      onDragRelease={(ev, gestureState) => changePosition(gestureState)}
-    >
-      <Svg
-        //height and width are size of jigsaw piece
-        height={widthY}
-        width={widthX}
+    <View style={{ flex: 1 }}>
+      <PanGestureHandler
+        // ref={moveRef}
+        // simultaneousHandlers={rotationRef}
+        onGestureEvent={onGestureEvent}
+        onHandlerStateChange={onHandlerStateChange}
       >
-        <Defs>
-          {/* for jigsaws, clip using piecePaths */}
-          <ClipPath id="jigsaw">
-            <Path d={piecePath} fill="white" stroke="white" />
-          </ClipPath>
-          {/* for squares, clipping is done by viewbox, rectangle = whole board */}
-          <ClipPath id="squares">
-            <Rect
-              fill="white"
-              stroke="white"
-              x={0}
-              y={0}
-              width={widthX}
-              height={widthY}
-            />
-          </ClipPath>
-        </Defs>
-        {/* <Path d={piecePath} stroke="white" /> */}
-
-        <Image
-          href={croppedImage}
-          width={widthX}
-          height={widthY}
-          clipPath={`url(#${puzzleType})`}
-        />
-      </Svg>
-    </Draggable>
+        <Animated.View
+          style={[
+            {
+              borderColor: "black",
+              borderWidth: 1,
+              width: 150,
+              height: 150,
+            },
+            {
+              transform: [
+                { translateX: pan.x },
+                { translateY: pan.y },
+                { rotate: rotateStr },
+                { perspective: 300 },
+              ],
+            },
+          ]}
+        >
+          <RotationGestureHandler
+            // ref={rotationRef}
+            // simultaneousHandlers={moveRef}
+            onGestureEvent={onRotateGestureEvent}
+            onHandlerStateChange={onRotateHandlerStateChange}
+          >
+            <AnimatedSvg height={150} width={150}>
+              <Image
+                href={{ uri: imageURI }}
+                width={150}
+                height={150}
+                // clipPath={`url(#${puzzleType})`}
+              />
+            </AnimatedSvg>
+          </RotationGestureHandler>
+        </Animated.View>
+      </PanGestureHandler>
+    </View>
   );
 };
