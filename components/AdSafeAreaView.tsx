81e431cd149dea300b824feaafff958d9be10367
Fu Goto <fu.goto@gmail.com>
2021-05-11 08:43:58 -0400

    first

--- components/AdSafeAreaView.tsx
+++ components/AdSafeAreaView.tsx
@@ -1,18 +1,26 @@
 import { AdMobBanner } from "expo-ads-admob";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { View } from "react-native";
 
 import { BANNER_ID } from "../constants";
 
 export default (props: any) => {
+  console.log('props', props.style)
   return (
     <SafeAreaView {...props.style}>
+      <View style={{minHeight: "0%", overflow: "hidden"}}>
       {props.children}
+
+      </View>
+
+      {/* {props.children.map(child => {return(<View style={{flex: 1}}>{child}</View>)})} */}
       <AdMobBanner
         bannerSize="smartBannerPortrait"
         adUnitID={BANNER_ID}
-        style={{ marginTop: "auto", alignSelf: "center" }}
+        style={{ marginTop: "auto", alignSelf: "center", flex: 2}}
       />
+    
     </SafeAreaView>
   );
 };
