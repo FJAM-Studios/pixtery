f540f9b60ce6812ade77c28f77c91bb20ee0975f
Fu Goto <fu.goto@gmail.com>
2021-05-11 08:46:14 -0400

    after linting

--- components/AdSafeAreaView.tsx
+++ components/AdSafeAreaView.tsx
@@ -6,21 +6,19 @@ import { View } from "react-native";
 import { BANNER_ID } from "../constants";
 
 export default (props: any) => {
-  console.log('props', props.style)
+  console.log("props", props.style);
   return (
     <SafeAreaView {...props.style}>
-      <View style={{minHeight: "0%", overflow: "hidden"}}>
-      {props.children}
-
+      <View style={{ minHeight: "0%", overflow: "hidden" }}>
+        {props.children}
       </View>
 
       {/* {props.children.map(child => {return(<View style={{flex: 1}}>{child}</View>)})} */}
       <AdMobBanner
         bannerSize="smartBannerPortrait"
         adUnitID={BANNER_ID}
-        style={{ marginTop: "auto", alignSelf: "center", flex: 2}}
+        style={{ marginTop: "auto", alignSelf: "center", flex: 2 }}
       />
-    
     </SafeAreaView>
   );
 };
