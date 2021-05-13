9863ffd4f273c4649f88ff97a1d9d738e704c877
Adam Reeves <aar2222@gmail.com>
2021-05-12 19:27:05 -0500

    handle error in case of invalid local storage

--- components/Puzzle.tsx
+++ components/Puzzle.tsx
@@ -73,6 +73,7 @@ export default ({
   useEffect(() => {
     const initializeSound = async () => {
       const { sound } = await Audio.Sound.createAsync(
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
         require("../assets/camera-click.wav")
       );
       setSound(sound);
@@ -84,8 +85,8 @@ export default ({
   useEffect(() => {
     return sound
       ? () => {
-        sound.unloadAsync();
-      }
+          sound.unloadAsync();
+        }
       : undefined;
   }, [sound]);
 
@@ -136,62 +137,70 @@ export default ({
       const shuffleOrder = shuffle(fillArray(gridSize), disableShuffle);
 
       const createPieces = async () => {
-        const _pieces: Piece[] = [];
-        const piecePaths =
-          puzzleType === "jigsaw"
-            ? generateJigsawPiecePaths(gridSize, squareSize)
-            : [];
-        // manipulate images in Puzzle component instead to save on renders
-        for (
-          let shuffledIndex = 0;
-          shuffledIndex < numPieces;
-          shuffledIndex++
-        ) {
-          const solvedIndex = shuffleOrder[shuffledIndex];
-          const {
-            pieceDimensions,
-            initialPlacement,
-            viewBox,
-            snapOffset,
-          } = getInitialDimensions(
-            puzzleType,
-            minSandboxY,
-            maxSandboxY,
-            solvedIndex,
-            shuffledIndex,
-            gridSize,
-            squareSize
-          );
+        try {
+          const _pieces: Piece[] = [];
+          const piecePaths =
+            puzzleType === "jigsaw"
+              ? generateJigsawPiecePaths(gridSize, squareSize)
+              : [];
+          // manipulate images in Puzzle component instead to save on renders
+          for (
+            let shuffledIndex = 0;
+            shuffledIndex < numPieces;
+            shuffledIndex++
+          ) {
+            const solvedIndex = shuffleOrder[shuffledIndex];
+            const {
+              pieceDimensions,
+              initialPlacement,
+              viewBox,
+              snapOffset,
+            } = getInitialDimensions(
+              puzzleType,
+              minSandboxY,
+              maxSandboxY,
+              solvedIndex,
+              shuffledIndex,
+              gridSize,
+              squareSize
+            );
 
-          const href = await ImageManipulator.manipulateAsync(
-            imageURI,
-            [
-              {
-                resize: {
-                  width: boardSize,
-                  height: boardSize,
+            const href = await ImageManipulator.manipulateAsync(
+              // testing an invalid imageURI, in case the pic is deleted/corrupted
+              // imageURI + "bogus",
+              imageURI,
+              [
+                {
+                  resize: {
+                    width: boardSize,
+                    height: boardSize,
+                  },
                 },
-              },
-              {
-                crop: { ...viewBox, ...pieceDimensions },
-              },
-            ],
-            { compress: 1, format: ImageManipulator.SaveFormat.JPEG }
-          );
+                {
+                  crop: { ...viewBox, ...pieceDimensions },
+                },
+              ],
+              { compress: 1, format: ImageManipulator.SaveFormat.JPEG }
+            );
 
-          const piece: Piece = {
-            href,
-            pieceDimensions,
-            piecePath: piecePaths.length ? piecePaths[solvedIndex] : "",
-            initialPlacement,
-            initialRotation:
-              Math.floor(Math.random() * 4) * 90 * DEGREE_CONVERSION,
-            solvedIndex,
-            snapOffset,
-          };
-          _pieces.push(piece);
+            const piece: Piece = {
+              href,
+              pieceDimensions,
+              piecePath: piecePaths.length ? piecePaths[solvedIndex] : "",
+              initialPlacement,
+              initialRotation:
+                Math.floor(Math.random() * 4) * 90 * DEGREE_CONVERSION,
+              solvedIndex,
+              snapOffset,
+            };
+            _pieces.push(piece);
+          }
+          setPieces(_pieces);
+        } catch (e) {
+          console.log(e);
+          alert("Could not load puzzle!");
+          navigation.navigate("Home");
         }
-        setPieces(_pieces);
       };
       createPieces();
       setSnapPoints(getSnapPoints(gridSize, squareSize));
