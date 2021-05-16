ff2b869f76a79ca643e25f393613fb93f295014e
Jeff <61642756+garber34@users.noreply.github.com>
2021-05-15 21:11:38 -0400

    thunks

--- store/reducers/receivedPuzzles.ts
+++ store/reducers/receivedPuzzles.ts
@@ -1,10 +1,17 @@
 import { AnyAction } from "redux";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import { Puzzle } from "../../types";
+
+async function saveToLocalStorage(newPuzzles: Puzzle[]){
+  await AsyncStorage.setItem("@pixteryPuzzles", JSON.stringify(newPuzzles));
+}
+
 
 // action types
 
 const SET_PUZZLES = "SET_PUZZLES";
+const DELETE_RECEIVED_PUZZLE = "DELETE_RECEIVED_PUZZLE";
 
 // action creators
 
@@ -15,6 +22,23 @@ export const setReceivedPuzzles = (receivedPuzzles: Puzzle[]): AnyAction => {
   };
 };
 
+const _deleteReceivedPuzzle = (receivedPuzzles: Puzzle[]): AnyAction => {
+  return {
+    type: DELETE_RECEIVED_PUZZLE,
+    receivedPuzzles,
+  };
+};
+
+//thunks
+export const deleteReceivedPuzzle = (publicKey: string): Function => {
+  return async function (dispatch, getState){
+    const newPuzzles = getState().receivedPuzzles.filter((puz) => puz.publicKey !== publicKey);
+    await AsyncStorage.setItem("@pixteryPuzzles", JSON.stringify(newPuzzles));
+    dispatch(_deleteReceivedPuzzle(newPuzzles));
+  };
+};
+
+
 // reducer
 
 const initialState: Puzzle[] = [];
@@ -22,6 +46,8 @@ const initialState: Puzzle[] = [];
 function reducer(state = initialState, action: AnyAction): Puzzle[] {
   switch (action.type) {
     case SET_PUZZLES:
+      return action.receivedPuzzles;
+    case DELETE_RECEIVED_PUZZLE:
       return action.receivedPuzzles;
     default:
       return state;
