16f833a3f27bbbe71860ca8fc5d87edb9132ee9b
Fu Goto <fu.goto@gmail.com>
2021-05-21 16:27:41 -0400

    first

--- components/AdSafeAreaView.tsx
+++ components/AdSafeAreaView.tsx
@@ -1,14 +1,16 @@
 import { AdMobBanner } from "expo-ads-admob";
-import React from "react";
+import React, { useState } from "react";
 import { StyleProp, ViewStyle } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { BANNER_ID } from "../constants";
-
+// start here - either do an onlayout, or do this
+// https://stackoverflow.com/questions/50935918/how-to-get-banner-size-of-smart-banner
 export default function AdSafeAreaView(props: {
   style: StyleProp<ViewStyle>;
   children: React.ReactNode;
 }): JSX.Element {
+  const [adHeight, setAdHeight] = useState(0);
   return (
     <SafeAreaView {...props.style}>
       {props.children}
