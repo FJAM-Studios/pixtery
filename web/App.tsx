595ffd7d4b35e7414e731dafb9520130330b221f
Adam Reeves <aar2222@gmail.com>
2021-06-02 17:15:05 -0500

    live app link on website

--- web/App.tsx
+++ web/App.tsx
@@ -49,9 +49,21 @@ export default function App(): JSX.Element {
     <div id="app">
       <img src="/logo.svg" className="logo" alt="Pixtery!" />
       <img src="/pixtery.svg" className="title" alt="Pixtery!" />
-      <h1 id="coming-soon">Coming Soon!</h1>
-      <img src="/app-store.svg" className="logo" alt="Pixtery!" />
-      <img src="/play-store.svg" className="logo" alt="Pixtery!" />
+      {/* <h1 id="coming-soon">Coming Soon!</h1> */}
+      <a
+        href="https://apps.apple.com/us/app/pixtery/id1569991739"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img src="/app-store.svg" className="logo" alt="Pixtery!" />
+      </a>
+      <a
+        href="https://play.google.com/store/apps/details?id=com.fjamstudios.pixtery"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img src="/play-store.svg" className="logo" alt="Pixtery!" />
+      </a>
       <div id="web-footer">
         <div id="web-footer-contents">
           <a href="https://pixtery.io/privacy.html">Privacy policy</a>
