31b3433ad31a568dac6aa8035d15072feb5a4b01
Adam Reeves <aar2222@gmail.com>
2021-10-16 19:20:41 -0500

    themes!

--- store/reducers/theme.ts
+++ store/reducers/theme.ts
@@ -1,8 +1,9 @@
 // the theme never changes so this is a little overkill but theme is getting passed down through multiple levels of components. Plus if we do decide to let the user switch between theme, e.g. day and night, this will make it easier.
 
 import { DefaultTheme } from "react-native-paper";
-import { Theme } from "react-native-paper/lib/typescript/types";
 import { AnyAction } from "redux";
+
+import { PixteryTheme } from "../../types";
 
 // action types
 
@@ -10,7 +11,7 @@ const SET_THEME = "SET_THEME";
 
 // action creators
 
-export const setTheme = (theme: Theme): AnyAction => {
+export const setTheme = (theme: PixteryTheme): AnyAction => {
   return {
     type: SET_THEME,
     theme,
@@ -19,9 +20,10 @@ export const setTheme = (theme: Theme): AnyAction => {
 
 // reducer
 
-const theme: Theme = {
+const defaultTheme: PixteryTheme = {
   ...DefaultTheme,
   roundness: 10,
+  name: "Pastel",
   colors: {
     ...DefaultTheme.colors,
     primary: "#7D8CC4",
@@ -35,9 +37,47 @@ const theme: Theme = {
   },
 };
 
-const initialState: Theme = theme;
+const darkTheme: PixteryTheme = {
+  ...DefaultTheme,
+  roundness: 10,
+  name: "Midnight",
+  dark: true,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "#808080",
+    accent: "#5C5C5C",
+    background: "#222222",
+    surface: "#808080",
+    text: "#f8f8ff",
+    disabled: "#808080",
+    placeholder: "#191102",
+    backdrop: "#191102",
+  },
+};
 
-function reducer(state = initialState, action: AnyAction): Theme {
+const lightTheme: PixteryTheme = {
+  ...DefaultTheme,
+  roundness: 10,
+  name: "Sunlight",
+  dark: false,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "#EFD9CE",
+    accent: "#CACACA",
+    background: "#F9F1D5",
+    surface: "#FFB868",
+    text: "#2f2f2f",
+    disabled: "#808080",
+    placeholder: "#FFE787",
+    backdrop: "#FFE787",
+  },
+};
+
+export const allThemes = [defaultTheme, darkTheme, lightTheme];
+
+const initialState: PixteryTheme = defaultTheme;
+
+function reducer(state = initialState, action: AnyAction): PixteryTheme {
   switch (action.type) {
     case SET_THEME:
       return action.theme;
