8943bd62d9a1c81a986027fd98754a6ffbdd3634
Adam Reeves <aar2222@gmail.com>
2021-10-20 14:09:55 -0500

    ask for app rating

--- components/AdSafeAreaView.tsx
+++ components/AdSafeAreaView.tsx
@@ -1,10 +1,12 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { AdMobBanner } from "expo-ads-admob";
+import * as StoreReview from "expo-store-review";
 import React, { useEffect } from "react";
 import { StyleProp, ViewStyle, View, LayoutChangeEvent } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useDispatch, useSelector } from "react-redux";
 
-import { BANNER_ID } from "../constants";
+import { BANNER_ID, DAY_IN_MILLISECONDS } from "../constants";
 import { setAdHeight } from "../store/reducers/adHeight";
 import { RootState } from "../types";
 
@@ -23,6 +25,35 @@ export default function AdSafeAreaView(props: {
   useEffect(() => {
     if (!adHeight) dispatch(setAdHeight(adHeight));
   }, [adHeight]);
+
+  useEffect(() => {
+    const checkReviewAvailable = async () => {
+      // verify app can ask for review
+      const hasAction = await StoreReview.hasAction();
+      const isAvail = await StoreReview.isAvailableAsync();
+
+      // check last time asked for review
+      let lastAskedReview = await AsyncStorage.getItem("@lastAskedReview");
+
+      // if never asked for review (like after initial install), then set
+      // last time asked as now and save
+      if (!lastAskedReview) {
+        lastAskedReview = Date.now().toString();
+        await AsyncStorage.setItem("@lastAskedReview", lastAskedReview);
+      }
+
+      // check if > 1 day since last asked
+      const askedRecently = Date.now() - +lastAskedReview > DAY_IN_MILLISECONDS;
+
+      // if app can ask and been more than 1 day, ask for review
+      if (hasAction && isAvail && !askedRecently) {
+        StoreReview.requestReview();
+        lastAskedReview = Date.now().toString();
+        await AsyncStorage.setItem("@lastAskedReview", lastAskedReview);
+      }
+    };
+    checkReviewAvailable();
+  }, []);
 
   return (
     <SafeAreaView {...props.style}>
