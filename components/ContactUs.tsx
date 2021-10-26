82c5486fb7a15c2a96aa53a2de4602418df71cfc
Fu Goto <fu.goto@gmail.com>
2021-10-25 23:35:40 -0400

    subject and email fields added

--- components/ContactUs.tsx
+++ components/ContactUs.tsx
@@ -1,23 +1,13 @@
 import React, { useState } from "react";
-import {
-  Button,
-  IconButton,
-  Text,
-  Surface,
-  Headline,
-  TextInput,
-  ActivityIndicator,
-  Modal,
-  Portal,
-} from "react-native-paper";
+import { Keyboard, Text } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { Button, TextInput } from "react-native-paper";
 import { useSelector } from "react-redux";
-import theme from "../store/reducers/theme";
+
+import { functions } from "../FirebaseApp";
 import { ScreenNavigation, RootState } from "../types";
 import AdSafeAreaView from "./AdSafeAreaView";
 import Header from "./Header";
-import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
-
-import { functions } from "../FirebaseApp";
 
 export default function ContactUs({
   navigation,
@@ -30,18 +20,21 @@ export default function ContactUs({
     (state: RootState) => state.receivedPuzzles
   );
   const [message, setMessage] = useState("");
+  const [subject, setSubject] = useState("");
+  const [email, setEmail] = useState("");
 
   const submit = async () => {
+    Keyboard.dismiss();
     const handleEmailCallable = functions.httpsCallable("handleEmail");
     try {
-      await handleEmailCallable({ message });
-      // handleEmailCallable().then((result) => console.log(result.data));
-      // console.log('result', result.data)
-      // return result;
-      // return puzzleData.data; // get just nested data from returned JSON
+      await handleEmailCallable({ subject, email, message });
+      setMessage("");
+      setEmail("");
+      setSubject("");
+      alert("Thank you! Your message has been successfully sent.");
     } catch (error) {
-      console.log("in contact us");
       console.error(error);
+      alert("Your message was not sent. Please try again.");
       throw new Error(error); //rethrow the error so it can be caught by outer method
     }
   };
@@ -65,24 +58,38 @@ export default function ContactUs({
       <KeyboardAwareScrollView
         resetScrollToCoords={{ x: 0, y: 0 }}
         keyboardShouldPersistTaps="handled"
-        // extraScrollHeight={
-        //   Platform.OS === "ios" ? 0 : buttonHeight + height * 0.2
-        // }
         enableOnAndroid
       >
+        <Text
+          style={{
+            color: "white",
+            fontSize: 20,
+            padding: 10,
+          }}
+        >
+          If you have any suggestions for features or encounter any bugs, please
+          let us know! We&apos;d love to hear from you.
+        </Text>
         <TextInput
           placeholder="Subject"
           multiline
-          // maxLength={messageLimit}
+          mode="outlined"
+          value={subject}
+          onChangeText={(subject) => setSubject(subject)}
+        />
+        <TextInput
+          placeholder="Your email"
+          multiline
+          mode="outlined"
+          value={email}
+          onChangeText={(email) => setEmail(email)}
+        />
+        <TextInput
+          placeholder="Write your message here!"
+          multiline
           mode="outlined"
           value={message}
           onChangeText={(message) => setMessage(message)}
-          // onFocus={() => setTextFocus(true)}
-          // onBlur={() => setTextFocus(false)}
-          style={{
-            minHeight: height * 0.09,
-            justifyContent: "center",
-          }}
         />
         <Button
           icon="send"
@@ -90,7 +97,6 @@ export default function ContactUs({
           onPress={submit}
           style={{ margin: height * 0.01 }}
           disabled={message.length === 0}
-          // onLayout={(ev) => setButtonHeight(ev.nativeEvent.layout.height)}
         >
           Submit Feedback
         </Button>
