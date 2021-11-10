7b0d833807a838ac7a5af4f975d454fc8c940ce5
Adam Reeves <aar2222@gmail.com>
2021-11-10 12:17:25 -0600

    fix formatting, edit secret msg

--- components/AddToGallery.tsx
+++ components/AddToGallery.tsx
@@ -7,7 +7,9 @@ import {
   View,
   ScrollView,
   TouchableOpacity,
+  Platform,
 } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import {
   ActivityIndicator,
   Button,
@@ -16,6 +18,7 @@ import {
   Headline,
   IconButton,
   Text,
+  TextInput,
 } from "react-native-paper";
 import Toast from "react-native-root-toast";
 import { useSelector } from "react-redux";
@@ -34,11 +37,12 @@ export default function AddToGallery({
   const receivedPuzzles = useSelector(
     (state: RootState) => state.receivedPuzzles
   );
-
+  const { height } = useSelector((state: RootState) => state.screenHeight);
   const sentPuzzles = useSelector((state: RootState) => state.sentPuzzles);
   const [modalVisible, setModalVisible] = useState(false);
   const [loading, setLoading] = useState(false);
   const [publicKey, setPublicKey] = useState<string>("");
+  const [message, setMessage] = useState<string>("");
   const [anonymousChecked, setAnonymousChecked] = useState(true);
 
   return (
@@ -57,158 +61,191 @@ export default function AddToGallery({
         }
         navigation={navigation}
       />
-      <ScrollView>
-        <Headline>Choose Gallery Submission</Headline>
-        <>
-          {sentPuzzles.length ? (
-            sentPuzzles.map((sentPuzzle, ix) => (
-              <TouchableOpacity
-                onPress={() => {
-                  setPublicKey(sentPuzzle.publicKey);
-                  setModalVisible(true);
-                }}
-                key={ix}
-              >
-                <Card
-                  style={{
-                    margin: 1,
-                    backgroundColor: theme.colors.surface,
-                  }}
-                >
-                  <Card.Title
-                    title={sentPuzzle.message || ""}
-                    subtitle={moment(sentPuzzle.dateReceived).calendar()}
-                    right={() => (
-                      <View
-                        style={{ flexDirection: "row", alignItems: "center" }}
-                      >
-                        <Text>{sentPuzzle.gridSize}</Text>
-                        <IconButton
-                          icon={
-                            sentPuzzle.puzzleType === "jigsaw"
-                              ? "puzzle"
-                              : "view-grid"
-                          }
-                        />
-                      </View>
-                    )}
-                    left={() => (
-                      <ImageBackground
-                        source={{
-                          uri:
-                            FileSystem.documentDirectory + sentPuzzle.imageURI,
-                        }}
-                        style={{
-                          flex: 1,
-                          justifyContent: "space-around",
-                          padding: 1,
-                        }}
-                      />
-                    )}
-                  />
-                </Card>
-              </TouchableOpacity>
-            ))
+      {modalVisible ? (
+        <KeyboardAwareScrollView
+          resetScrollToCoords={{ x: 0, y: 0 }}
+          keyboardShouldPersistTaps="handled"
+          extraScrollHeight={Platform.OS === "ios" ? 0 : height * 0.2}
+          enableOnAndroid
+          style={{
+            backgroundColor: theme.colors.backdrop,
+            flex: 1,
+            // width: "100%",
+            // height: "100%",
+            borderRadius: theme.roundness,
+            // position: "absolute",
+
+            padding: 10,
+          }}
+          contentContainerStyle={{
+            alignItems: "center",
+          }}
+        >
+          {loading ? (
+            <ActivityIndicator
+              animating
+              color={theme.colors.text}
+              size="large"
+            />
           ) : (
-            <View
-              style={{
-                alignItems: "center",
-              }}
-            >
-              <Headline>You haven&apos;t sent any puzzles!</Headline>
-            </View>
-          )}
-        </>
-        {modalVisible ? (
-          <View
-            style={{
-              backgroundColor: theme.colors.backdrop,
-              width: "100%",
-              height: "100%",
-              borderRadius: theme.roundness,
-              position: "absolute",
-              alignItems: "center",
-              justifyContent: "space-evenly",
-              padding: 10,
-            }}
-          >
-            {loading ? (
-              <ActivityIndicator
-                animating
-                color={theme.colors.text}
-                size="large"
-              />
-            ) : (
-              <>
-                <Headline>Confirm?</Headline>
-                <Text style={{ textAlign: "center" }}>
-                  This is legalese that says we can use your Pixtery. But also
-                  we might not.
-                </Text>
-                <View
-                  style={{
-                    flexDirection: "row",
-                    alignContent: "center",
-                    margin: 10,
-                  }}
-                >
-                  <Checkbox
-                    status={anonymousChecked ? "checked" : "unchecked"}
-                    color={theme.colors.surface}
-                    onPress={() => {
-                      setAnonymousChecked(!anonymousChecked);
-                    }}
-                  />
-                  <Text>
-                    Submit Anonymously (Your Pixtery name will not be included)
-                  </Text>
-                </View>
-                <Button
-                  mode="contained"
-                  icon="cancel"
+            <>
+              <Headline>Confirm?</Headline>
+              <Text style={{ textAlign: "center" }}>
+                This is legalese that says we can use your Pixtery. But also we
+                might not.
+              </Text>
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignContent: "center",
+                  margin: 10,
+                }}
+              >
+                <Checkbox
+                  status={anonymousChecked ? "checked" : "unchecked"}
+                  color={theme.colors.surface}
                   onPress={() => {
-                    setPublicKey("");
-                    setModalVisible(false);
+                    setAnonymousChecked(!anonymousChecked);
                   }}
-                >
-                  Cancel
-                </Button>
-                <Button
-                  mode="contained"
-                  icon="check"
-                  onPress={async () => {
-                    setLoading(true);
-                    const addToQueue = functions.httpsCallable("addToQueue");
-                    try {
-                      await addToQueue({ publicKey, anonymousChecked });
-                      Toast.show(
-                        "Thanks for your submission! The Pixtery team will review soon!",
-                        {
-                          duration: Toast.durations.LONG,
-                          position: Toast.positions.CENTER,
-                        }
-                      );
-                    } catch (e) {
-                      if (e instanceof Error) {
-                        console.log(e.message);
-                      }
-                      Toast.show("We're sorry, something went wrong :(", {
+                />
+                <Text>
+                  Submit Anonymously (Your Pixtery name will not be included)
+                </Text>
+              </View>
+              <View
+                style={{
+                  // flexDirection: "row",
+                  alignContent: "center",
+                  margin: 10,
+                }}
+              >
+                <Text>Edit secret message:</Text>
+                <TextInput
+                  multiline
+                  maxLength={70}
+                  mode="outlined"
+                  value={message}
+                  onChangeText={(message) => setMessage(message)}
+                  outlineColor={theme.colors.primary}
+                  placeholderTextColor={theme.colors.primary}
+                  style={{ justifyContent: "center", width: "100%" }}
+                />
+              </View>
+              <Button
+                mode="contained"
+                icon="cancel"
+                onPress={() => {
+                  setPublicKey("");
+                  setMessage("");
+                  setModalVisible(false);
+                }}
+                style={{
+                  margin: 10,
+                }}
+              >
+                Cancel
+              </Button>
+              <Button
+                mode="contained"
+                icon="check"
+                onPress={async () => {
+                  setLoading(true);
+                  const addToQueue = functions.httpsCallable("addToQueue");
+                  try {
+                    await addToQueue({ publicKey, anonymousChecked, message });
+                    Toast.show(
+                      "Thanks for your submission! The Pixtery team will review soon!",
+                      {
                         duration: Toast.durations.LONG,
                         position: Toast.positions.CENTER,
-                      });
+                      }
+                    );
+                  } catch (e) {
+                    if (e instanceof Error) {
+                      console.log(e.message);
                     }
-                    navigation.navigate("Gallery");
-                    setLoading(false);
-                    setModalVisible(false);
+                    Toast.show("We're sorry, something went wrong :(", {
+                      duration: Toast.durations.LONG,
+                      position: Toast.positions.CENTER,
+                    });
+                  }
+                  navigation.navigate("Gallery");
+                  setLoading(false);
+                  setModalVisible(false);
+                }}
+              >
+                Submit To Gallery
+              </Button>
+            </>
+          )}
+        </KeyboardAwareScrollView>
+      ) : (
+        <ScrollView>
+          <Headline>Choose Gallery Submission</Headline>
+          <>
+            {sentPuzzles.length ? (
+              sentPuzzles.map((sentPuzzle, ix) => (
+                <TouchableOpacity
+                  onPress={() => {
+                    setPublicKey(sentPuzzle.publicKey);
+                    setMessage(sentPuzzle.message || "");
+                    setModalVisible(true);
                   }}
+                  key={ix}
                 >
-                  Submit To Gallery
-                </Button>
-              </>
+                  <Card
+                    style={{
+                      margin: 1,
+                      backgroundColor: theme.colors.surface,
+                    }}
+                  >
+                    <Card.Title
+                      title={sentPuzzle.message || ""}
+                      subtitle={moment(sentPuzzle.dateReceived).calendar()}
+                      right={() => (
+                        <View
+                          style={{ flexDirection: "row", alignItems: "center" }}
+                        >
+                          <Text>{sentPuzzle.gridSize}</Text>
+                          <IconButton
+                            icon={
+                              sentPuzzle.puzzleType === "jigsaw"
+                                ? "puzzle"
+                                : "view-grid"
+                            }
+                          />
+                        </View>
+                      )}
+                      left={() => (
+                        <ImageBackground
+                          source={{
+                            uri:
+                              FileSystem.documentDirectory +
+                              sentPuzzle.imageURI,
+                          }}
+                          style={{
+                            flex: 1,
+                            justifyContent: "space-around",
+                            padding: 1,
+                          }}
+                        />
+                      )}
+                    />
+                  </Card>
+                </TouchableOpacity>
+              ))
+            ) : (
+              <View
+                style={{
+                  alignItems: "center",
+                }}
+              >
+                <Headline>You haven&apos;t sent any puzzles!</Headline>
+              </View>
             )}
-          </View>
-        ) : null}
-      </ScrollView>
+          </>
+        </ScrollView>
+      )}
     </AdSafeAreaView>
   );
 }
