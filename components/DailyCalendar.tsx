5d34310303913321bcc44e4b7eeff59e9c328810
Adam Reeves <aar2222@gmail.com>
2021-11-15 15:36:45 -0600

    remove duplicate code

--- components/DailyCalendar.tsx
+++ components/DailyCalendar.tsx
@@ -1,11 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { View } from "react-native";
-import { Calendar } from "react-native-calendars";
-import { DateData } from "react-native-calendars/src/types";
 import { Headline, IconButton } from "react-native-paper";
 import { useSelector } from "react-redux";
 
-import { functions } from "../FirebaseApp";
 import { RootState, ScreenNavigation } from "../types";
 import AdSafeAreaView from "./AdSafeAreaView";
 import DateSelect from "./DateSelect";
@@ -21,35 +18,6 @@ export default function DailyCalendar({
     (state: RootState) => state.receivedPuzzles
   );
 
-  const [markedDates, setMarkedDates] = useState<any>({});
-
-  const loadDailies = async (monthData: DateData) => {
-    try {
-      const getDailyDates = functions.httpsCallable("getDailyDates");
-      const res = await getDailyDates(monthData);
-      const foundDailies = res.data;
-      const newDates: any = {};
-      for (let i = 0; i < foundDailies.length; i++) {
-        const daily = foundDailies[i];
-        newDates[daily.dailyDate] = { selected: true, puzzle: daily };
-      }
-      setMarkedDates(newDates);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-  useEffect(() => {
-    const today = new Date();
-    const firstDay = new Date(today.getFullYear(), today.getMonth(), 1);
-    const monthData: DateData = {
-      dateString: firstDay.toISOString().split("T")[0],
-      day: firstDay.getDay(),
-      month: firstDay.getMonth() + 1,
-      year: firstDay.getFullYear(),
-      timestamp: 0,
-    };
-    loadDailies(monthData);
-  }, []);
   return (
     <AdSafeAreaView
       style={{
