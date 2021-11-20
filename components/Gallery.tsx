ef765833dfbbc05bec6fcfc1859dc363d4d0dd76
Adam Reeves <aar2222@gmail.com>
2021-11-19 18:26:50 -0600

    countdown to next daily pixtery

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -8,7 +8,7 @@ import { useSelector } from "react-redux";
 import { functions } from "../FirebaseApp";
 import { INTERSTITIAL_ID } from "../constants";
 import { Puzzle, RootState, ScreenNavigation } from "../types";
-import { downloadImage } from "../util";
+import { downloadImage, msToTime } from "../util";
 import AdSafeAreaView from "./AdSafeAreaView";
 import Header from "./Header";
 
@@ -26,23 +26,46 @@ export default function Gallery({
   const { boardSize } = useSelector((state: RootState) => state.screenHeight);
   const [loading, setLoading] = useState(true);
   const [daily, setDaily] = useState<Puzzle | null>(null);
-  useEffect(() => {
-    const loadDaily = async () => {
-      const getDaily = functions.httpsCallable("getDaily");
-      try {
-        const today = new Date().toISOString().split("T")[0];
-        const res = await getDaily({ today });
-        const daily = res.data[0];
-        if (daily) {
-          await downloadImage(daily);
-          setDaily(daily);
-        }
-      } catch (e) {
-        console.log(e);
+  const [time, setTime] = useState<null | number>(null);
+
+  const getCountdown = () => {
+    const now = new Date();
+    const tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    tomorrow.setUTCHours(0, 0, 0, 0);
+    // uncomment the code below to test time/date change, simulates being 1 min before end of day
+    //now.setUTCHours(24, -1);
+    const time = tomorrow.getTime() - now.getTime();
+    if (time <= 1000) loadDaily();
+    setTime(time);
+  };
+
+  const loadDaily = async () => {
+    setLoading(true);
+    const getDaily = functions.httpsCallable("getDaily");
+    try {
+      const today = new Date().toISOString();
+      const res = await getDaily({ today });
+      const daily = res.data[0];
+      if (daily) {
+        // think we don't need to do this if not showing blurred image here
+        // await downloadImage(daily);
+        setDaily(daily);
       }
-      setLoading(false);
-    };
+    } catch (e) {
+      console.log(e);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    const incrementTime = setInterval(() => {
+      getCountdown();
+    }, 1000);
+
     loadDaily();
+
+    return () => clearInterval(incrementTime);
   }, []);
   return (
     <AdSafeAreaView
@@ -69,9 +92,6 @@ export default function Gallery({
         }}
       >
         <Headline>Daily Pixtery</Headline>
-        {!loading && daily ? (
-          <Text style={{ fontSize: 20 }}>Touch to solve!</Text>
-        ) : null}
         <View style={{ flex: 1, alignContent: "center", margin: 10 }}>
           {loading ? (
             <ActivityIndicator size="large" />
@@ -101,26 +121,66 @@ export default function Gallery({
                   }
                 );
                 try {
-                  await AdMobInterstitial.requestAdAsync();
-                  await AdMobInterstitial.showAdAsync();
+                  //make it so we don't have to watch ads in dev
+                  if (process.env.NODE_ENV !== "development") {
+                    await AdMobInterstitial.requestAdAsync();
+                    await AdMobInterstitial.showAdAsync();
+                  } else {
+                    navigation.navigate("AddPuzzle", {
+                      publicKey,
+                      sourceList: "received",
+                    });
+                  }
                 } catch (error) {
+                  // go to the puzzle if there's an ad error
+                  navigation.navigate("AddPuzzle", {
+                    publicKey,
+                    sourceList: "received",
+                  });
                   console.log(error);
                 }
               }}
             >
-              <Image
-                source={{
-                  uri: FileSystem.documentDirectory + daily.imageURI,
-                }}
+              <View
                 style={{
                   width: boardSize * 0.8,
                   height: boardSize * 0.8,
+                  alignItems: "center",
+                  justifyContent: "space-evenly",
+                  backgroundColor: "grey",
+                  borderRadius: theme.roundness,
+                  opacity: 0.8,
                 }}
-                blurRadius={boardSize * 0.03}
-              />
+              >
+                <Text style={{ fontSize: 20 }}>Touch to solve!</Text>
+                <Text style={{ fontSize: 20 }}>
+                  Today&apos;s Pixtery expires in:
+                </Text>
+                {time ? (
+                  <Text style={{ fontSize: 20 }}>{msToTime(time)}</Text>
+                ) : null}
+              </View>
             </TouchableOpacity>
           ) : (
-            <Text>We must be asleep! No Daily today :(</Text>
+            <View
+              style={{
+                width: boardSize * 0.8,
+                height: boardSize * 0.8,
+                alignItems: "center",
+                justifyContent: "space-evenly",
+                backgroundColor: "grey",
+                borderRadius: theme.roundness,
+                opacity: 0.8,
+              }}
+            >
+              <Text style={{ fontSize: 20 }}>
+                We must be asleep! No Daily today :(
+              </Text>
+              <Text style={{ fontSize: 20 }}>Check back in:</Text>
+              {time ? (
+                <Text style={{ fontSize: 20 }}>{msToTime(time)}</Text>
+              ) : null}
+            </View>
           )}
         </View>
         <View
