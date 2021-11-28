2b091e887171672d6be7e4ecea9fc239d0bb54ce
adam-adair <aar2222@gmail.com>
2021-11-28 18:32:11 -0500

    fix merge

--- functions/src/fns/galleryFns.ts
+++ functions/src/fns/galleryFns.ts
@@ -2,7 +2,7 @@ import * as functions from "firebase-functions";
 import db from "../db";
 
 export const checkGalleryAdmin = functions.https.onCall(
-  async (data, context) => {
+  async (_data, context): Promise<boolean | void> => {
     try {
       // throw error if user is not authenticated
       if (!context.auth) {
@@ -21,14 +21,15 @@ export const checkGalleryAdmin = functions.https.onCall(
         return false;
       }
       return true;
-    } catch (error: any) {
-      throw new functions.https.HttpsError("unknown", error.message, error);
+    } catch (error: unknown) {
+      if (error instanceof Error)
+        throw new functions.https.HttpsError("unknown", error.message, error);
     }
   }
 );
 
 export const addToGallery = functions.https.onCall(
-  async (data, context): Promise<Record<string, any> | void> => {
+  async (data, context): Promise<void> => {
     try {
       // throw error if user is not authenticated
       if (!context.auth) {
@@ -77,61 +78,65 @@ export const addToGallery = functions.https.onCall(
         .collection("galleryQueue")
         .doc(publicKey)
         .set({ active: false }, { merge: true });
-    } catch (error: any) {
-      throw new functions.https.HttpsError("unknown", error.message, error);
+    } catch (error: unknown) {
+      if (error instanceof Error)
+        throw new functions.https.HttpsError("unknown", error.message, error);
     }
   }
 );
 
-export const getDailyDates = functions.https.onCall(async (data, context) => {
-  try {
-    // throw error if user is not authenticated
-    if (!context.auth) {
-      throw new functions.https.HttpsError(
-        "permission-denied",
-        "user not authenticated"
-      );
-    }
-    // throw error if user is not member of gallery admins
-    const admin = await db
-      .collection("galleryAdmins")
-      .doc(context.auth.uid)
-      .get();
-    if (!admin.exists) {
-      throw new functions.https.HttpsError(
-        "permission-denied",
-        "user not gallery admin"
-      );
-    }
-    const { month, year } = data;
-    const dailies = await db
-      .collection("gallery")
-      .doc(year)
-      .collection(month)
-      .get();
+export const getDailyDates = functions.https.onCall(
+  async (data, context): Promise<void | FirebaseFirestore.DocumentData> => {
+    try {
+      // throw error if user is not authenticated
+      if (!context.auth) {
+        throw new functions.https.HttpsError(
+          "permission-denied",
+          "user not authenticated"
+        );
+      }
+      // throw error if user is not member of gallery admins
+      const admin = await db
+        .collection("galleryAdmins")
+        .doc(context.auth.uid)
+        .get();
+      if (!admin.exists) {
+        throw new functions.https.HttpsError(
+          "permission-denied",
+          "user not gallery admin"
+        );
+      }
+      const { month, year } = data;
+      const dailies = await db
+        .collection("gallery")
+        .doc(year)
+        .collection(month)
+        .get();
 
-    if (!dailies.empty) {
-      // this will include whatever data we want the user to submit with the puzzle
-      // i.e., sender name vs anonymous, tags, title, etc.
-      // that will need to be built out in the gallery submission form.
-      // for now, it only includes the normal puzzle data
-      const dailyPuzzlesForTheMonth = dailies.docs.map((doc) => {
-        const puzzleData = doc.data();
-        const day = doc.id;
-        return {
-          puzzleData,
-          day,
-        };
-      });
-      return dailyPuzzlesForTheMonth;
-    } else return [];
-  } catch (error: any) {
-    throw new functions.https.HttpsError("unknown", error.message, error);
+      if (!dailies.empty) {
+        // this will include whatever data we want the user to submit with the puzzle
+        // i.e., sender name vs anonymous, tags, title, etc.
+        // that will need to be built out in the gallery submission form.
+        // for now, it only includes the normal puzzle data
+        const dailyPuzzlesForTheMonth = dailies.docs.map((doc) => {
+          const puzzleData = doc.data();
+          const day = doc.id;
+          return {
+            puzzleData,
+            day,
+          };
+        });
+        return dailyPuzzlesForTheMonth;
+      } else return [];
+    } catch (error: unknown) {
+      if (error instanceof Error)
+        throw new functions.https.HttpsError("unknown", error.message, error);
+    }
   }
-});
+);
 
 export const removeDailyPuzzle = functions.https.onCall(
-  async (data, context): Promise<Record<string, any> | void> => {
+  async (data, context): Promise<void> => {
     try {
       // throw error if user is not authenticated
       if (!context.auth) {
@@ -165,35 +170,39 @@ export const removeDailyPuzzle = functions.https.onCall(
         .collection("galleryQueue")
         .doc(publicKey)
         .set({ active: true }, { merge: true });
-    } catch (error: any) {
-      throw new functions.https.HttpsError("unknown", error.message, error);
+    } catch (error: unknown) {
+      if (error instanceof Error)
+        throw new functions.https.HttpsError("unknown", error.message, error);
     }
   }
 );
 
-export const getDaily = functions.https.onCall(async (data, context) => {
-  try {
-    // throw error if user is not authenticated
-    if (!context.auth) {
-      throw new functions.https.HttpsError(
-        "permission-denied",
-        "user not authenticated"
-      );
-    }
-    const { year, month, day } = data;
-    const daily = await db
-      .collection("gallery")
-      .doc(year)
-      .collection(month)
-      .doc(day)
-      .get();
+export const getDaily = functions.https.onCall(
+  async (data, context): Promise<void | FirebaseFirestore.DocumentData> => {
+    try {
+      // throw error if user is not authenticated
+      if (!context.auth) {
+        throw new functions.https.HttpsError(
+          "permission-denied",
+          "user not authenticated"
+        );
+      }
+      const { year, month, day } = data;
+      const daily = await db
+        .collection("gallery")
+        .doc(year)
+        .collection(month)
+        .doc(day)
+        .get();
 
-    if (daily.exists) return daily.data();
-    else {
-      // @todo - once a day cloud function that populates from last years puzzle
-      return [];
+      if (daily.exists) return daily.data();
+      else {
+        // @todo - once a day cloud function that populates from last years puzzle
+        return [];
+      }
+    } catch (error: unknown) {
+      if (error instanceof Error)
+        throw new functions.https.HttpsError("unknown", error.message, error);
     }
-  } catch (error: any) {
-    throw new functions.https.HttpsError("unknown", error.message, error);
   }
-});
+);
