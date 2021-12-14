66b18e6907012cd8eae58a6569a309ede2546402
Fu Goto <fu.goto@gmail.com>
2021-12-14 00:38:33 -0500

    countdown visual

---
+++ components/Timer.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import { Animated } from "react-native";
+import { CountdownCircleTimer } from "react-native-countdown-circle-timer";
+import { useSelector } from "react-redux";
+
+import { RootState } from "../types";
+import { secondsToTime } from "../util";
+
+export default function Timer({ time }: { time: number }): JSX.Element {
+  const { width } = useSelector((state: RootState) => state.screenHeight);
+
+  return (
+    <CountdownCircleTimer
+      isPlaying
+      duration={60 * 60 * 24}
+      initialRemainingTime={time / 1000}
+      colors={[
+        ["#004777", 0.4],
+        ["#F7B801", 0.4],
+        ["#A30000", 0.2],
+      ]}
+      size={width * 0.6}
+    >
+      {({ remainingTime, animatedColor }) => (
+        <Animated.Text style={{ color: animatedColor, fontSize: 40 }}>
+          {secondsToTime(remainingTime)}
+        </Animated.Text>
+      )}
+    </CountdownCircleTimer>
+  );
+}
