a7358e1cc99286c22e077da76345dfef99f08ca7
mxavier927 <mxavier927@gmail.com>
2022-01-23 21:58:33 -0500

    updated placeholder message, input settings and display

--- components/InteractiveElements/MessageInput.tsx
+++ components/InteractiveElements/MessageInput.tsx
@@ -6,18 +6,17 @@ import { useSelector } from "react-redux";
 import { RootState } from "../../types";
 
 export default function MessageInput({
-  style,
   message,
   setMessage,
 }: {
-  style: {
-    height: number;
-    margin: number;
-  };
   message: string;
   setMessage: React.Dispatch<React.SetStateAction<string>>;
 }): JSX.Element {
   const theme = useSelector((state: RootState) => state.theme);
+  const height =
+    useSelector((state: RootState) => state.screenHeight.height) * 0.09;
+  const margin =
+    useSelector((state: RootState) => state.screenHeight.height) * 0.01;
   const [messageFocus, setMessageFocus] = React.useState(false);
   const messageLimit = 70;
   const messageDraft = message.length > 0;
@@ -25,10 +24,10 @@ export default function MessageInput({
   return (
     <View
       style={{
-        marginTop: style.margin,
-        marginLeft: style.margin,
-        marginRight: style.margin,
-        maxHeight: style.height,
+        marginTop: margin,
+        marginLeft: margin,
+        marginRight: margin,
+        maxHeight: height,
         justifyContent: "center",
       }}
     >
@@ -37,43 +36,18 @@ export default function MessageInput({
           flexDirection: "row",
           justifyContent: "space-between",
           alignItems: "center",
-        }}
-      >
-        <Text
-          style={{
-            textAlign: "left",
-          }}
-        >
-          Secret Message:
-        </Text>
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-          }}
-        >
-          <Text
-            style={{
-              textAlign: "right",
-            }}
-          >
-            {message.length}/{messageLimit} characters
-          </Text>
-        </View>
-      </View>
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
           borderWidth: messageFocus ? 2.5 : 1,
-          borderRadius: theme.roundness,
+          borderTopLeftRadius: theme.roundness,
+          borderTopRightRadius: theme.roundness,
+          borderBottomLeftRadius: messageFocus ? 0 : theme.roundness,
+          borderBottomRightRadius: messageFocus ? 0 : theme.roundness,
           borderColor: theme.colors.primary,
           padding: 5,
           marginTop: 2.5,
         }}
       >
         <TextInput
-          placeholder="Congrats! You solved the puzzle!"
+          placeholder="Message (optional, reveals when solved)"
           multiline
           maxLength={messageLimit}
           value={message}
@@ -83,7 +57,6 @@ export default function MessageInput({
           onBlur={() => setMessageFocus(false)}
           style={{
             color: theme.colors.text,
-            margin: 0,
             justifyContent: "center",
             backgroundColor: theme.colors.background,
             width: "90%",
@@ -101,6 +74,21 @@ export default function MessageInput({
           }}
         />
       </View>
+      {messageFocus ? (
+        <Text
+          style={{
+            textAlign: "right",
+            alignItems: "center",
+            backgroundColor: theme.colors.primary,
+            borderBottomLeftRadius: theme.roundness,
+            borderBottomRightRadius: theme.roundness,
+            paddingRight: 5,
+            paddingBottom: 2.5,
+          }}
+        >
+          {message.length}/{messageLimit} characters
+        </Text>
+      ) : null}
     </View>
   );
 }
