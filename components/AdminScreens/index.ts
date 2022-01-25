cbf7abb87afbecddf141f1591e256abcd3ed52df
adam-adair <aar2222@gmail.com>
2022-01-25 18:58:19 -0500

    move A2G screen, change Tutorial wording

--- components/AdminScreens/index.ts
+++ components/AdminScreens/index.ts
@@ -1,6 +1,5 @@
-import AddToGallery from "./AddToGallery";
 import DailyCalendar from "./DailyCalendar";
 import GalleryQueue from "./GalleryQueue";
 import GalleryReview from "./GalleryReview";
 
-export { AddToGallery, DailyCalendar, GalleryQueue, GalleryReview };
+export { DailyCalendar, GalleryQueue, GalleryReview };
