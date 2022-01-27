dd2c507963c9105ec7cea6bf84c41c8f11c1f3e7
Jeff <61642756+garber34@users.noreply.github.com>
2022-01-26 20:04:50 -0500

    cloud functions

--- functions/src/fns/userFns.ts
+++ functions/src/fns/userFns.ts
@@ -1,5 +1,17 @@
+import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
 import db from "../db";
+
+// throw error if user is not authenticated
+const blockIfNotAuthenticated = (context: functions.https.CallableContext) => {
+  console.log("CHECK IF AUTHENTICATED");
+  if (!context.auth) {
+    throw new functions.https.HttpsError(
+      "permission-denied",
+      "user not authenticated"
+    );
+  }
+};
 
 // send a user's entire sent or received list (listType) to the client
 export const fetchPuzzles = functions.https.onCall(
@@ -35,17 +47,10 @@ export const deactivateAllUserPuzzles = functions.https.onCall(
   async (list, context) => {
     console.log("deactivating user puzzle");
     try {
-      // throw error if user is not authenticated
-      if (!context.auth) {
-        throw new functions.https.HttpsError(
-          "permission-denied",
-          "user not authenticated"
-        );
-      }
-
+      blockIfNotAuthenticated(context);
       // mark userPuzzle as removed from user's list
       db.collection("userPixteries")
-        .doc(context.auth.uid)
+        .doc(context!.auth!.uid)
         .collection(list)
         .get()
         .then(function (querySnapshot) {
@@ -67,17 +72,10 @@ export const deactivateUserPuzzle = functions.https.onCall(
     const { publicKey, list } = data;
     console.log("deactivating user puzzle");
     try {
-      // throw error if user is not authenticated
-      if (!context.auth) {
-        throw new functions.https.HttpsError(
-          "permission-denied",
-          "user not authenticated"
-        );
-      }
-
+      blockIfNotAuthenticated(context);
       // mark userPuzzle as removed from user's list
       db.collection("userPixteries")
-        .doc(context.auth.uid)
+        .doc(context!.auth!.uid)
         .collection(list)
         .doc(publicKey)
         .set({ active: false }, { merge: true });
@@ -112,18 +110,58 @@ const copyList = async (
   });
 };
 
+const deleteUserDoc = async (uid: string) => {
+  try {
+    db.collection("userPixteries")
+      .doc(uid)
+      .delete()
+      .then(function () {
+        console.log("Document successfully deleted!");
+      })
+      .catch(function (error) {
+        throw new functions.https.HttpsError("unknown", error.message, error);
+      });
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new functions.https.HttpsError("unknown", error.message, error);
+    }
+  }
+};
+
+const deleteUserAuth = (uid: string) => {
+  try {
+    console.log("DELETE!");
+    admin.auth().deleteUser(uid);
+    console.log("DELETED");
+  } catch (error) {
+    throw new functions.https.HttpsError("unknown", error.message, error);
+  }
+};
+
 export const migratePuzzles = functions.https.onCall(
   async (prevUserId: string, context) => {
     try {
       // throw error if user is not authenticated
-      if (!context.auth) {
-        throw new functions.https.HttpsError(
-          "permission-denied",
-          "user not authenticated"
-        );
-      }
+      blockIfNotAuthenticated(context);
       copyList(prevUserId, context!.auth!.uid, "sent");
       copyList(prevUserId, context!.auth!.uid, "received");
+      console.log("MIGRATING!");
+      deleteUserDoc(prevUserId);
+      deleteUserAuth(prevUserId);
+    } catch (error) {
+      if (error instanceof Error) {
+        throw new functions.https.HttpsError("unknown", error.message, error);
+      }
+    }
+  }
+);
+
+export const deleteUser = functions.https.onCall(
+  async (context: functions.https.CallableContext) => {
+    try {
+      blockIfNotAuthenticated(context);
+      deleteUserDoc(context!.auth!.uid);
+      deleteUserAuth(context!.auth!.uid);
     } catch (error) {
       if (error instanceof Error) {
         throw new functions.https.HttpsError("unknown", error.message, error);
