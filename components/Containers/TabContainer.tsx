4738fd41a48c4fdcb93487f5b4e203894651ec39
Jeff <61642756+garber34@users.noreply.github.com>
2022-02-05 17:43:23 -0500

    create fake status bar

--- components/Containers/TabContainer.tsx
+++ components/Containers/TabContainer.tsx
@@ -1,7 +1,8 @@
 import { MaterialCommunityIcons, Ionicons } from "@expo/vector-icons";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import React from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { View } from "react-native";
+import { SafeAreaView, useSafeAreaInsets } from "react-native-safe-area-context";
 import { useSelector } from "react-redux";
 
 import { RootState, StackScreens } from "../../types";
@@ -16,76 +17,82 @@ const Tab = createMaterialTopTabNavigator<StackScreens>();
 export default function TabContainer(): JSX.Element {
   const theme = useSelector((state: RootState) => state.theme);
   const profile = useSelector((state: RootState) => state.profile);
+  const insets = useSafeAreaInsets();
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: theme.colors.primary }}>
-      <Tab.Navigator
-        initialRouteName="MakeContainer"
-        screenOptions={({ route }) => ({
-          tabBarAllowFontScaling: true,
-          tabBarContentContainerStyle: {
-            backgroundColor: theme.colors.primary,
-          },
-          tabBarLabelStyle: {
-            color: theme.colors.text,
-          },
-          tabBarIcon: ({ focused }) => {
-            let iconName: React.ComponentProps<
-              typeof MaterialCommunityIcons
-            >["name"] = "alert-circle";
-            if (route.name === "MakeContainer") iconName = "camera-iris";
-            if (route.name === "DailyContainer")
-              iconName = "white-balance-sunny";
-            if (route.name === "LibraryContainer") iconName = "book-multiple";
-            if (route.name === "SettingsContainer")
+    <>
+      <View
+        style={{ height: insets.top, backgroundColor: theme.colors.primary }}
+      />
+      <SafeAreaView
+        edges={["right", "bottom", "left"]}
+        style={{ flex: 1, backgroundColor: theme.colors.background }}
+      >
+        <Tab.Navigator
+          initialRouteName="MakeContainer"
+          screenOptions={({ route }) => ({
+            tabBarAllowFontScaling: true,
+            tabBarContentContainerStyle: {
+              backgroundColor: theme.colors.primary,
+            },
+            tabBarLabelStyle: {
+              color: theme.colors.text,
+            },
+            tabBarIcon: ({ focused }) => {
+              let iconName: React.ComponentProps<
+                typeof MaterialCommunityIcons
+              >["name"] = "alert-circle";
+              if (route.name === "MakeContainer") iconName = "camera-iris";
+              if (route.name === "DailyContainer")
+                iconName = "white-balance-sunny";
+              if (route.name === "LibraryContainer") iconName = "book-multiple";
+              if (route.name === "SettingsContainer")
+                return (
+                  <Ionicons
+                    size={24}
+                    name="settings"
+                    color={focused ? theme.colors.text : theme.colors.onSurface}
+                  />
+                );
+              if (route.name === "AdminContainer") iconName = "lock";
               return (
-                <Ionicons
+                <MaterialCommunityIcons
                   size={24}
-                  name="settings"
+                  name={iconName}
                   color={focused ? theme.colors.text : theme.colors.onSurface}
                 />
               );
-            if (route.name === "AdminContainer") iconName = "lock";
-            return (
-              <MaterialCommunityIcons
-                size={24}
-                name={iconName}
-                color={focused ? theme.colors.text : theme.colors.onSurface}
-              />
-            );
-          },
-        })}
-      >
-        <Tab.Screen
-          name="MakeContainer"
-          component={MakeContainer}
-          options={{ title: "Make" }}
-        />
-        <Tab.Screen
-          name="DailyContainer"
-          component={DailyContainer}
-          options={{ tabBarLabel: "Daily" }}
-        />
-        <Tab.Screen
-          name="LibraryContainer"
-          component={LibraryContainer}
-          options={{ tabBarLabel: "Library" }}
-        />
-        <Tab.Screen
-          name="SettingsContainer"
-          component={SettingsContainer}
-          options={{ tabBarLabel: "Settings" }}
-        />
-        {profile && profile.isGalleryAdmin ? (
+            },
+          })}
+        >
           <Tab.Screen
-            name="AdminContainer"
-            component={AdminContainer}
-            options={{ tabBarLabel: "Admin" }}
+            name="MakeContainer"
+            component={MakeContainer}
+            options={{ title: "Make" }}
           />
-        ) : null}
-      </Tab.Navigator>
-      <SafeAreaView
-        style={{ flex: 0, backgroundColor: theme.colors.background }}
-      />
-    </SafeAreaView>
+          <Tab.Screen
+            name="DailyContainer"
+            component={DailyContainer}
+            options={{ tabBarLabel: "Daily" }}
+          />
+          <Tab.Screen
+            name="LibraryContainer"
+            component={LibraryContainer}
+            options={{ tabBarLabel: "Library" }}
+          />
+          <Tab.Screen
+            name="SettingsContainer"
+            component={SettingsContainer}
+            options={{ tabBarLabel: "Settings" }}
+          />
+          {profile && profile.isGalleryAdmin ? (
+            <Tab.Screen
+              name="AdminContainer"
+              component={AdminContainer}
+              options={{ tabBarLabel: "Admin" }}
+            />
+          ) : null}
+        </Tab.Navigator>
+      </SafeAreaView>
+    </>
   );
 }
