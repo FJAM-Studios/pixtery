9e62dc82c358d91bf51162044cb29936ed697ae9
Fu Goto <fu.goto@gmail.com>
2022-02-13 12:29:53 -0500

    daily agt language

--- components/StaticElements/DailyAgreement.tsx
+++ components/StaticElements/DailyAgreement.tsx
@@ -15,7 +15,8 @@ export default function DailyAgreement(): JSX.Element {
         }}
       >
         Before submitting, please make sure your Pixtery is appropriate for all
-        audiences. We do not condone hateful or offensive content of any kind. Failure to comply may result in disabled accounts or other legal action.
+        audiences. We do not condone hateful or offensive content of any kind.
+        Failure to comply may result in disabled accounts or other legal action.
       </Text>
       <Text
         style={{
