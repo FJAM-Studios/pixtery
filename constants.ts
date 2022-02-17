51c722722d2ee338241314fe06d9ff09b8487dc8
adam-adair <aar2222@gmail.com>
2022-02-17 08:40:25 -0500

    bump build number

--- constants.ts
+++ constants.ts
@@ -35,7 +35,7 @@ const PUBLIC_KEY_LENGTH = 9;
 
 const MIN_BOTTOM_CLEARANCE = 0.7;
 
-const VERSION_NUMBER = "1.2.0";
+const VERSION_NUMBER = "2.0.1";
 
 const ARGUABLY_CLEVER_PHRASES = [
   "Please wait!",
