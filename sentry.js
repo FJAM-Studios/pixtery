5369dbaf089e7e7fa2e23b310f04114e4220677e
Mark Guinn <66528804+mavirex@users.noreply.github.com>
2022-05-18 19:29:01 -0400

    Delete sentry.js

--- sentry.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  AUTH_TOKEN:
-    "779d3ccff12b4cb1ac26a7a2ae1b851f22cc9a1a0ac441a3945c1345b2cc2879",
-  DSN:
-    "https://c8e5645714bf4f05b4c7a397d5d99bb3@o1207021.ingest.sentry.io/6340472",
-};
