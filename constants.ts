4eafbd5edd3083afd0c894e746fc5af4708eb3c4
adam-adair <aar2222@gmail.com>
2022-09-12 15:05:22 -0400

    vsn bump

--- constants.ts
+++ constants.ts
@@ -39,7 +39,7 @@ const PUBLIC_KEY_LENGTH = 9;
 
 const MIN_BOTTOM_CLEARANCE = 0.7;
 
-const VERSION_NUMBER = "2.0.54";
+const VERSION_NUMBER = "2.0.55";
 
 const ARGUABLY_CLEVER_PHRASES = [
   "Please wait!",
